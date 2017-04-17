86f86de361e6c39e1b1db93f02cb544b4a5eb94a
Gabriel <gabrielmacus@gmail.com>
2017-04-17 12:39:00 -0300

    repositorios

--- includes/classes/schema/Archivo/ArchivoDAO.php
+++ includes/classes/schema/Archivo/ArchivoDAO.php
@@ -116,9 +116,12 @@ archivo_id=:archivo_id, archivo_size=:archivo_size,archivo_mime=:archivo_mime, a
        // echo json_encode($ftp->put("/httpdocs/data/2017/04/12/1492008967.jpg/1492008967_original.jpg","C:/xampp5/htdocs/adhoc-framework/tmp/files/606453_7up.jpg",FTP_BINARY));
     //    exit();
 
+  /*      echo $fullDir." ".$a->getTmpPath();
 
-
-        if(!$ftp->put($fullDir,$a->getTmpPath()))
+        exit();
+*/
+        $ftp->pasv(true);
+        if(!$ftp->put($fullDir,$a->getTmpPath(),FTP_BINARY))
         {
             throw new Exception("ArchivoDAO:0");
         }
