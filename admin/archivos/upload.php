559fe25f5c1734df479a2eb1efc7ee19b445418c
gabrielmacus <gabrielmacus@gmail.com>
2017-04-20 00:20:58 -0300

    posts

--- admin/archivos/upload.php
+++ admin/archivos/upload.php
@@ -5,9 +5,33 @@
  * Date: 04/04/2017
  * Time: 01:30 PM
  */
+/*
 include "../../includes/autoload.php";
 
-;
+include_once DIR_PATH."/extras/api/check-login.php";
+$tmps=array();
+
+foreach ($_FILES as $file)
+{
+
+    $dest=DIR_PATH."tmp/files/{$file["size"]}_{$file["name"]}";
+
+    move_uploaded_file($file["tmp_name"],$dest);
+
+    $tmp="/tmp/files/{$file["size"]}_{$file["name"]}";
+    $dest=$configuracion->getSiteAddress().$tmp;
+
+$ext = explode(".",$file["name"]);
+$ext = end($ext);
+    array_push($tmps,
+        array("url"=>$dest,"name"=>$file["name"],"size"=>$file["size"],"type"=>$ext,"mime"=>$file["type"],"tmp"=>$tmp));
+
+}
+echo  json_encode($tmps);
+*/
+
+include "../../includes/autoload.php";
+
 include_once DIR_PATH."/extras/api/check-login.php";
 $tmps=array();
 
@@ -18,11 +42,11 @@ foreach ($_FILES as $file)
 
     move_uploaded_file($file["tmp_name"],$dest);
 
-    $tmp="/tmp/files/{$file["size"]}_{$file["name"]}";
+    $tmp="{$_SERVER['DOCUMENT_ROOT']}/tmp/files/{$file["size"]}_{$file["name"]}";
     $dest=$configuracion->getSiteAddress().$tmp;
 
-$ext = explode(".",$file["name"]);
-$ext = end($ext);
+    $ext = explode(".",$file["name"]);
+    $ext = end($ext);
     array_push($tmps,
         array("url"=>$dest,"name"=>$file["name"],"size"=>$file["size"],"type"=>$ext,"mime"=>$file["type"],"tmp"=>$tmp));
 
