119a0865f4d028c672f768966042d73d99c73325
gabrielmacus <gabrielmacus@gmail.com>
2017-04-22 00:40:42 -0300

    implementacion de bulma framework

--- includes/panel/templates/comun/sidenav.php
+++ includes/panel/templates/comun/sidenav.php
@@ -32,7 +32,7 @@ foreach ($nav as $item)
 {
     ?>
     <li  class="item animated <?php echo $item["active"]?>">
-        <a>
+        <a <?php if ($item["href"]){ ?> href="<?php echo $item["href"]?>"<?php }?>>
            <!-- href=""--->
             <span class="icon <?php echo $colorPallete[random_int(0,4)]?> "> <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
             <span class="text"><?php echo $item["text"]?></span>
@@ -41,7 +41,7 @@ foreach ($nav as $item)
 {
     ?>
         <ul style="display: none" class=" main-color">
-            <?php iterateSidenav($item["items"]); ?>
+            <?php  ?>
         </ul>
     <?php
 }?>
