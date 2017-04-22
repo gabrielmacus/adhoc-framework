d4808d3c82e133aa2e25c1e163390e14dc49c660
gabrielmacus <gabrielmacus@gmail.com>
2017-04-21 23:12:10 -0300

    implementacion de bulma framework

--- includes/autoload.php
+++ includes/autoload.php
@@ -24,17 +24,11 @@ $configuracion = new Configuracion("localhost","c0580153_adhoc","Mapuche17","c05
     "uipasd",0.1,"http://localhost/adhoc-framework","Adhoc Framework","Gamaware Web Tech",
     "test", "http://mapucheonline.com/test","http://mapucheonline.com/test");
 
-/*
-$configuracion = new Configuracion("173.236.78.206","test","sercan02","adhoc",
-    "uipasd",0.1,"http://localhost/adhoc-framework","Adhoc Framework","Gamaware Web Tech",
-    "adhoc-framework", "http://localhost/adhoc-framework","http://localhost/adhoc-framework");*/
 
-
-/*
 $configuracion = new Configuracion("localhost","c0580153_adhoc","Mapuche17","c0580153_adhoc",
     "uipasd",0.1,"http://mapucheonline.com/test/admin","Adhoc Framework","Gamaware Web Tech",
     "","http://mapucheonline.com/test/admin","http://mapucheonline.com/test/admin");
-*/
+
 require_once "classes/autoload-classes.php";//Cargo las clases
 
 require_once "helpers/autoload-helpers.php";//Cargo las funciones utiles
