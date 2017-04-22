3fa2a22ab6d19393cff15d0ffb7bccfedafb2dd3
gabrielmacus <gabrielmacus@gmail.com>
2017-04-22 00:38:50 -0300

    implementacion de bulma framework

--- includes/panel/templates/comun/sidenav.php
+++ includes/panel/templates/comun/sidenav.php
@@ -1,1 +1,61 @@
-<?php var_dump($lang); ?>
+
+<script>
+    $(document).on("click",".item",openSubmenu);
+
+    function openSubmenu(e) {
+
+        var item = $(e.target).closest(".item");
+        if(item)
+        {
+
+
+            item.find("ul").fadeToggle(400,function () {
+                item.toggleClass("active");
+
+            });
+
+        }
+
+
+       }
+
+
+
+
+</script>
+
+<?php
+
+
+function iterateSidenav($nav)
+{$colorPallete=["saffron","shamrock","honey-flower","alizarin","belize-hole"];
+foreach ($nav as $item)
+{
+    ?>
+    <li  class="item animated <?php echo $item["active"]?>">
+        <a <?php if ($item["href"]){ ?> href="<?php echo $item["href"]?>"<?php }?>>
+           <!-- href="<?php echo $item["href"]?>"--->
+            <span class="icon <?php echo $colorPallete[random_int(0,4)]?> "> <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+            <span class="text"><?php echo $item["text"]?></span>
+        </a>
+        <?php if ($item["items"])
+{
+    ?>
+        <ul style="display: none" class=" main-color">
+            <?php iterateSidenav($item["items"]); ?>
+        </ul>
+    <?php
+}?>
+    </li>
+
+    <?php
+}
+}
+?>
+
+<ul class="sidenav main-color">
+    <li class="title"><a><?php echo $configuracion->getSiteName()." ".$configuracion->getVersion() ?></a></li>
+
+    <?php iterateSidenav($lang["sidenav"]) ?>
+
+</ul>
