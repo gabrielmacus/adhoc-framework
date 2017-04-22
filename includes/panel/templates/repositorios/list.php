81bb31a55b126bede2d2a39ecfdb6768f40d5ea2
gabrielmacus <gabrielmacus@gmail.com>
2017-04-22 02:10:24 -0300

    implementacion de bulma framework

--- includes/panel/templates/repositorios/list.php
+++ includes/panel/templates/repositorios/list.php
@@ -6,7 +6,12 @@
 <div class="body">
 
     <ul class="list">
-        <li class="item"><a class="animated">Repositorio 1 <i class="fa fa-pencil icon" aria-hidden="true"></i></a></li>
+        <li class="item">
+            <a class="animated">Repositorio 1
+                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                <i class="fa fa-pencil icon" aria-hidden="true"></i>
+            </a>
+        </li>
         <li class="item"><a class="animated">Repositorio 2</a></li>
         <li class="item"><a class="animated">Repositorio 3</a></li>
     </ul>
