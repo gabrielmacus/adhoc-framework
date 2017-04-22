f44fea4b49da52e6df9ffff55afba26911ce8e10
gabrielmacus <gabrielmacus@gmail.com>
2017-04-22 01:38:55 -0300

    implementacion de bulma framework

--- includes/panel/templates/secciones/list.php
+++ includes/panel/templates/secciones/list.php
@@ -12,15 +12,15 @@ scope.$apply();
     <h2><?php echo $lang["secciones"]?></h2>
 </header>
 <script type="text/ng-template" id="categoryTree">
-    {{ value.nombre }}
+
     <ul ng-if="value.secciones">
-        <li ng-repeat="category in value.secciones" ng-include="'categoryTree'">
+        <li ng-repeat="category in value.secciones" ng-include="'categoryTree'">{{ value.nombre }}
         </li>
     </ul>
 </script>
 
 <ul>
-    <li data-ng-repeat="(key,value) in secciones"  ng-include="'categoryTree'"></li>
+    <li data-ng-repeat="(key,value) in secciones"  ng-include="'categoryTree'">{{ value.nombre }}</li>
 </ul>
 <div class="body">
 
