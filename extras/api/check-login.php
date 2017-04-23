aa082507cfa4082805d05c078febe2ed54191bb3
gabrielmacus <gabrielmacus@gmail.com>
2017-04-23 18:04:09 -0300

    implementacion de bulma framework

--- extras/api/check-login.php
+++ extras/api/check-login.php
@@ -25,7 +25,7 @@ try
 catch (Exception $e)
 {
     $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    setcookie("referer",$actual_link);
+    setcookie("referer",$actual_link,0,"/");
 
     if(!$redirect)
     {
