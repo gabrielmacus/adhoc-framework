73bdc04aa0528cffa7d06ed0bca676b8f6a320c2
gabrielmacus <gabrielmacus@gmail.com>
2017-04-23 17:19:39 -0300

    implementacion de bulma framework

--- admin/posts/map.php
+++ admin/posts/map.php
@@ -1,7 +1,18 @@
 <script async defer
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAmTWqtcbhdnNXdv-Ow60PedAtYwYgbQ5Q&callback=initMap">
 </script>
-
+<style>
+    map {
+        height: 100%;
+        width: 100%;
+        float: left;
+    }
+    html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+</style>
 <script>
 
     function initMap() {
