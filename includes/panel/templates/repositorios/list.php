527fddad7ab94ab1ee21103caa09808d995eff0d
gabrielmacus <gabrielmacus@gmail.com>
2017-04-23 00:00:00 -0300

    implementacion de bulma framework

--- includes/panel/templates/repositorios/list.php
+++ includes/panel/templates/repositorios/list.php
@@ -24,7 +24,7 @@
 
         <li class="item" data-ng-repeat="(k,r) in repositorios">
             <a class="animated">
-                <span class="name">{{r.titulo}}</span>
+                <span class="name">{{r.name}}</span>
                 <i class="fa fa-pencil icon animated" aria-hidden="true"></i>
                 <i class="fa fa-trash-o icon animated" aria-hidden="true"></i>
             </a>
