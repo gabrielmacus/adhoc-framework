3ab5291167396e2382fffbe43541e014b5a21e95
gabrielmacus <gabrielmacus@gmail.com>
2017-04-23 18:08:49 -0300

    implementacion de bulma framework

--- admin/hoyos/index.php
+++ admin/hoyos/index.php
@@ -17,31 +17,13 @@ $htmlLocality="Paraná,Entre Rios";
 try{
 
 
-    $post = new Post();
-    $post->setTitulo($_POST["titulo"]);
-    $post->setTexto($_POST["texto"]);
-    $post->setArchivos($_POST["archivos"]);
-    $post->setSeccion($_POST["seccion"]);
-    $post->setBajada($_POST["bajada"]);
-    $post->setVolanta($_POST["volanta"]);
-    $post->setExtra1($_POST["extra_1"]);
-    $post->setExtra2($_POST["extra_2"]);
-    $post->setExtra3($_POST["extra_3"]);
-    $post->setExtra4($_POST["extra_4"]);
-    $post->setAnexos($_POST["anexos"]);
-    //$post->setAnexos(array(array("post_anexo_id"=>24)));
-
-
-    if($_POST["id"])
+    if(is_numeric($_GET["id"]))
     {
-
-        $post->setId($_POST["id"]);
-        echo json_encode($GLOBALS["postDAO"]->updatePost($post));
+        $post=$GLOBALS["postDAO"]->selectPostById($_GET["id"]);
     }
-    else
-    { echo json_encode($GLOBALS["postDAO"]->insertPost($post));
 
-    }
+    $list="hoyos";
+    $action="add";
 
 
 
---
+++ includes/panel/templates/hoyos/add.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Puers
+ * Date: 23/04/2017
+ * Time: 18:06
+ */
