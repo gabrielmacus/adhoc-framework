c25f1eeabc01b4ebda8aece1749c60f8aa3320bf
gabrielmacus <gabrielmacus@gmail.com>
2017-04-23 12:45:15 -0300

    implementacion de bulma framework

--- includes/panel/templates/posts/table.php
+++ includes/panel/templates/posts/table.php
@@ -8,6 +8,12 @@
         <tr>
             <th scope="col"><?php echo $k?></th>
         </tr>
+        <tr>
+            <th scope="col"><?php echo $lang["editar"];?></th>
+        </tr>
+        <tr>
+            <th scope="col"><?php echo $lang["eliminar"];?></th>
+        </tr>
     <?php
     }?>
 
