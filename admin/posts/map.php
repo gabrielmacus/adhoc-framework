d97861fc99cb84fd0ee31cfd92e544062d3e54c3
gabrielmacus <gabrielmacus@gmail.com>
2017-04-23 17:20:58 -0300

    implementacion de bulma framework

--- admin/posts/map.php
+++ admin/posts/map.php
@@ -1,3 +1,13 @@
+<!doctype html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+</head>
+<body>
 <script async defer
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAmTWqtcbhdnNXdv-Ow60PedAtYwYgbQ5Q&callback=initMap">
 </script>
@@ -30,3 +40,5 @@
 <map style="width: 100%;height: 300px">
 
 </map>
+</body>
+</html>
