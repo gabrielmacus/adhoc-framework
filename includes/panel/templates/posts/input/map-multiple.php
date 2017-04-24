a3b444cdf1991b6472f498ffa0b3a54f9388e3ab
gabrielmacus <gabrielmacus@gmail.com>
2017-04-24 02:56:14 -0300

    implementacion de bulma framework

--- includes/panel/templates/posts/input/map-multiple.php
+++ includes/panel/templates/posts/input/map-multiple.php
@@ -18,7 +18,7 @@
         {
             ?>
 
-            locations = scope.post;
+            locations = scope.post.<?php echo $model?>;
 
             console.log(locations);
             $.each(locations,function (k,v) {
