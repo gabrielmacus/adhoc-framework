fe53fabb6647ad684d68107c314309023d70c6fc
gabrielmacus <gabrielmacus@gmail.com>
2017-04-24 18:34:51 -0300

    demo

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php
 
+/**
+ * Created by PhpStorm.
+ * User: Puers
+ * Date: 03/04/2017
+ * Time: 1:07
+ */
+
+
 var_dump("AASD");
 
+include "includes/autoload.php";
+
+include_once DIR_PATH."/extras/api/check-login.php";
+
+$htmlTitle=$configuracion->getSiteName();
+$htmlKeywords="keyword";
+$htmlDescription="Descriptiom";
+$htmlLocality="Paraná,Entre Rios";
+
+try{
+    $site="home";
+    $action="index";
+
+
+}
+catch (Exception $e)
+{
+
+    echo json_encode("Error: {$e->getMessage()}");
+
+}
+
+include DIR_PATH."/includes/site/templates/comun/estructura.php";
