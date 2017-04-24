f7fbc34cf313519784fe5fddb2d38324930301fa
Gabriel <gabrielmacus@gmail.com>
2017-04-24 13:20:03 -0300

    posts

---
+++ includes/panel/templates/jugadores/save.php
@@ -0,0 +1,9 @@
+<?php
+$label="Nombre";
+$model="titulo";
+include DIR_PATH."/includes/panel/templates/posts/input/text.php";
+
+$label="Apellido";
+$model="volanta";
+include DIR_PATH."/includes/panel/templates/posts/input/text.php";
+
