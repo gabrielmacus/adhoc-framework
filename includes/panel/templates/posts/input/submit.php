bdd1406d849fa4c86349e55b315de110234320ee
Gabriel <gabrielmacus@gmail.com>
2017-04-24 12:51:57 -0300

    posts

--- includes/panel/templates/posts/input/submit.php
+++ includes/panel/templates/posts/input/submit.php
@@ -1,4 +1,4 @@
 <div class="form-block">
-    <input data-ng-click="<?php echo $action;?>" type="submit animated" value="<?php echo $name;?>">
+    <input data-ng-click="<?php echo $action;?>" type="submit" class="animated" value="<?php echo $name;?>">
 </div>
 
