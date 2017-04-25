8ae0ff18c32f84b0bdf6be20158fb20aac0d23e8
gabrielmacus <gabrielmacus@gmail.com>
2017-04-25 01:44:53 -0300

    demo

--- includes/panel/templates/archivos/list.php
+++ includes/panel/templates/archivos/list.php
@@ -6,6 +6,12 @@
 $label="Archivos";
 $id="archivos1";
 include DIR_PATH."/includes/panel/templates/posts/input/files.php";
+
+
+
+$label="Archivos 2";
+$id="archivos2";
+include DIR_PATH."/includes/panel/templates/posts/input/files.php";
 ?>
 
     </form>
