51a4cae2e9d36c672184dae57cae0c584f8373d5
gabrielmacus <gabrielmacus@gmail.com>
2017-04-24 23:12:51 -0300

    demo

--- includes/panel/templates/posts/files.php
+++ includes/panel/templates/posts/files.php
@@ -1,14 +1,14 @@
 <div class="files">
 
     <div class="file-preview" data-ng-repeat="p in previews">
-
+<!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
         <div class="file" >
 
             <figure>
                 <img data-src="{{p.url}}">
             </figure>
 
-            <span class="name">{{p.}}</span>
+            <span class="name">{{p.name}}</span>
             <span class="size">{{p.size}}</span>
         </div>
 
