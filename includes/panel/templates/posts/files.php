5595824dbec7df1700ea748840cff56154dad738
gabrielmacus <gabrielmacus@gmail.com>
2017-04-24 23:15:27 -0300

    demo

--- includes/panel/templates/posts/files.php
+++ includes/panel/templates/posts/files.php
@@ -1,6 +1,6 @@
 <div class="files">
 
-    <div class="file-preview" data-ng-repeat="p in previews" data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'">
+    <div class="file-preview s12 m6 l4" data-ng-repeat="p in previews" data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'">
 <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
         <div class="file" >
 
