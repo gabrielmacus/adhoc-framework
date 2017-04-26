8b156b786a6116721c60454a04c4f4965b4bcf51
gabrielmacus <gabrielmacus@gmail.com>
2017-04-26 01:59:09 -0300

    paginador

--- includes/panel/templates/repositorios/list.php
+++ includes/panel/templates/repositorios/list.php
@@ -126,8 +126,8 @@
         <li class="item" data-ng-repeat="(k,r) in repositorios">
             <a class="animated">
                 <span class="name">{{r.nombre}}</span>
-                <a class="icon animated" href="<?php echo DIR_PATH."/admin/repositorios/?id="?>{{r.id}}"><i class="fa fa-pencil  " aria-hidden="true"></i></a>
-                <i class="fa fa-trash-o icon animated" aria-hidden="true"></i>
+                <a class="icon animated" href="<?php echo $configuracion->getSiteAddress()."/admin/repositorios/?id="?>{{r.id}}"><i class="fa fa-pencil  " aria-hidden="true"></i></a>
+                <a class="icon animated"><i class="fa fa-trash-o icon animated" aria-hidden="true"></i></a>
             </a>
             <div class="content">
 
