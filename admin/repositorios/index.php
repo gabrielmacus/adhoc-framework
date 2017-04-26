dd2a46b87ec075db1dd92f34de3c6e1f10e7a92a
gabrielmacus <gabrielmacus@gmail.com>
2017-04-26 00:20:18 -0300

    paginador

--- admin/repositorios/index.php
+++ admin/repositorios/index.php
@@ -23,7 +23,7 @@ try{
         $versionPanel="thumbnail";
        $p =is_numeric( $_GET["p"])?$_GET["p"]: 0;
 
-        $GLOBALS["archivoDAO"]->setLimit(3);
+        $GLOBALS["archivoDAO"]->setLimit(1);
         $GLOBALS["archivoDAO"]->setActualPage($p);
 
         $archivos= $GLOBALS["archivoDAO"]->selectArchivoByRepositorioId($_GET["id"]);
