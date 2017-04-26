a877a9b71aa74d47db569243d889297c276f073b
Gabriel <gabrielmacus@gmail.com>
2017-04-26 17:35:40 -0300

    paginador

--- includes/panel/templates/posts/paginador.php
+++ includes/panel/templates/posts/paginador.php
@@ -28,11 +28,6 @@ $paginador=$paginador->getPaginador();
             <a class="animated <?php echo $p["class"]?>" href="?<?php  echo $qs."&p=".$p["number"]?>"><?php echo $p["number"] ?></a>
             <?php
         }?>
-        <?php if($actualPage<$paginador->getPages())
-        {
-            ?>
-            <a class="animated next" href="?<?php  echo $qs."&p=".($actualPage+1)?>">&raquo;</a>
-            <?Php
-        }?>
+     
     </div>
 </div>
