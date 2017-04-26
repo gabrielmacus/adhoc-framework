ba6c09d68c81c2e539d11ee0a024fd71c5730250
Gabriel <gabrielmacus@gmail.com>
2017-04-26 15:47:16 -0300

    repositorios

--- includes/panel/templates/posts/paginador.php
+++ includes/panel/templates/posts/paginador.php
@@ -1,12 +1,12 @@
 <div class="fila center">
     <div class="pagination">
         <a class="animated" href="#">&laquo;</a>
-        <a class="animated" href="#">1</a>
-        <a class="animated" href="#">2</a>
-        <a class="animated" href="#">3</a>
-        <a class="animated" href="#">4</a>
-        <a class="animated" href="#">5</a>
-        <a class="animated" href="#">6</a>
+        <?php  foreach ($paginador as $p)
+        {
+            ?>
+            <a class="animated <?php echo $p["class"]?>" href="#"><?php echo $p["number"] ?></a>
+            <?php
+        }?>
         <a class="animated" href="#">&raquo;</a>
     </div>
 </div>
