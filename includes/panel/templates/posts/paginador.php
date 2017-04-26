07aa2b0a32b065484b9158f87f68b9cd60a9e895
Gabriel <gabrielmacus@gmail.com>
2017-04-26 16:30:00 -0300

    paginador

--- includes/panel/templates/posts/paginador.php
+++ includes/panel/templates/posts/paginador.php
@@ -8,7 +8,7 @@
         foreach ($paginador as $p)
         {
             ?>
-            <a class="animated <?php echo $p["class"]?>" href="?<?php  echo $qs."&".$p["number"]?>"><?php echo $p["number"] ?></a>
+            <a class="animated <?php echo $p["class"]?>" href="?<?php  echo $qs."&p=".$p["number"]?>"><?php echo $p["number"] ?></a>
             <?php
         }?>
         <a class="animated" href="#">&raquo;</a>
