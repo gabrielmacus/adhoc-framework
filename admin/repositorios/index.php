55f76349aec7868c9cfba43f4a4f173d2e83710c
gabrielmacus <gabrielmacus@gmail.com>
2017-04-25 22:10:42 -0300

    demo

--- admin/repositorios/index.php
+++ admin/repositorios/index.php
@@ -21,9 +21,12 @@ try{
     if(is_numeric($_GET["id"]))
     {
 
+        $p =is_numeric( $_GET["p"])?$_GET["p"]: 0;
 
+        $GLOBALS["archivoDAO"]->setLimit(2);
+        $GLOBALS["archivoDAO"]->setActualPage($p);
 
-        $archivos= $GLOBALS["archivoDAO"]->selectArchivoByRepositorio($_GET["id"]);
+        $archivos= $GLOBALS["archivoDAO"]->selectArchivoByRepositorioId($_GET["id"]);
         $site="archivos";
     }
     else
