1a58381fffb83dabb98b1dd47e8bbc03b770d4d4
Gabriel <gabrielmacus@gmail.com>
2017-04-26 15:44:31 -0300

    repositorios

--- includes/panel/templates/posts/paginador.php
+++ includes/panel/templates/posts/paginador.php
@@ -1,12 +1,12 @@
 <div class="fila center">
     <div class="pagination">
-        <a href="#">&laquo;</a>
-        <a href="#">1</a>
-        <a href="#">2</a>
-        <a href="#">3</a>
-        <a href="#">4</a>
-        <a href="#">5</a>
-        <a href="#">6</a>
-        <a href="#">&raquo;</a>
+        <a class="animated" href="#">&laquo;</a>
+        <a class="animated" href="#">1</a>
+        <a class="animated" href="#">2</a>
+        <a class="animated" href="#">3</a>
+        <a class="animated" href="#">4</a>
+        <a class="animated" href="#">5</a>
+        <a class="animated" href="#">6</a>
+        <a class="animated" href="#">&raquo;</a>
     </div>
 </div>
