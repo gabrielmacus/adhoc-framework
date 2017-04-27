636fc4efd0a817406b718ce73edd73b117613530
gabrielmacus <gabrielmacus@gmail.com>
2017-04-27 00:06:38 -0300

    paginador

--- includes/panel/templates/posts/input/files.php
+++ includes/panel/templates/posts/input/files.php
@@ -68,7 +68,7 @@
     position: relative;
     text-align: center;"><?php echo $uploadMessage;?></span>
 
-        <i style="position: absolute;top: 20px;right: 20px;color: #0d71bb" class="fa fa-cloud-upload" aria-hidden="true"></i>
+        <i style="position: absolute;top: 20px;right: 20px;font-size: 30px;color: #0d71bb" class="fa fa-cloud-upload" aria-hidden="true"></i>
 
     </div>
 
