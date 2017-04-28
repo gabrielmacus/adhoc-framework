69e7e76c7bc5fe5cf2d9208e1b8b9fcabe959690
gabrielmacus <gabrielmacus@gmail.com>
2017-04-28 02:52:37 -0300

    secciones

--- includes/panel/templates/comun/sidenav.php
+++ includes/panel/templates/comun/sidenav.php
@@ -62,7 +62,7 @@ foreach ($nav as $item)
 }
 ?>
 
-<i style="position: fixed;top: 10px;right: 10px" class="fa fa-times" aria-hidden="true"></i>
+<i style="position: fixed;top: 10px;right: 10px;font-size: 30px" class="fa fa-times" aria-hidden="true"></i>
 
 
 <ul class="sidenav  main-color">
