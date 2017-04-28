240fb58f8da921da24b4972e58df0793e53d7cba
gabrielmacus <gabrielmacus@gmail.com>
2017-04-28 02:06:52 -0300

    secciones

--- includes/panel/templates/comun/menu.php
+++ includes/panel/templates/comun/menu.php
@@ -1,5 +1,5 @@
 <nav class="navigation main-color">
-    <a class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></a>
+    <a class="hamburger hide-l"><i class="fa fa-bars" aria-hidden="true"></i></a>
     <a class="item"></a>
     <a class="title"></a>
 </nav>
