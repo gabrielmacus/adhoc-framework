05166d4023a1fd0b4cc185ba01f773f4ac28eebf
Gabriel <gabrielmacus@gmail.com>
2017-04-28 14:02:27 -0300

    Pendiente: agregar usuarios con respectivos permisos

--- includes/site/templates/comun/estructura.php
+++ includes/site/templates/comun/estructura.php
@@ -27,6 +27,50 @@ include DIR_PATH."/includes/panel/templates/comun/loader.php"; ?>
     var app = angular.module('panel', []);
     app.config(function ($provide, $httpProvider) {
 
+        // Intercept http calls.
+        $provide.factory('httpInterceptor', function ($q) {
+            return {
+                // On request success
+                request: function (config) {
+
+                    $(".loader").removeClass("active");
+                    // console.log(config); // Contains the data about the request before it is sent.
+
+                    // Return the config or wrap it in a promise if blank.
+                    return config || $q.when(config);
+                },
+
+                // On request failure
+                requestError: function (rejection) {
+                    // console.log(rejection); // Contains the data about the error on the request.
+
+                    // Return the promise rejection.
+                    return $q.reject(rejection);
+                },
+
+                // On response success
+                response: function (response) {
+                    // console.log(response); // Contains the data from the response.
+                    $(".loader").addClass("active");
+                    $(".loader .info").html("");
+                    // Return the response or promise.
+
+                    return response || $q.when(response);
+                },
+
+                // On response failture
+                responseError: function (rejection) {
+                    // console.log(rejection); // Contains the data about the error.
+                    $(".loader").addClass("active");
+                    $(".loader .info").html("");
+                    // Return the promise rejection.
+                    return $q.reject(rejection);
+                }
+            };
+        });
+
+        // Add the interceptor to the $httpProvider.
+        $httpProvider.interceptors.push('httpInterceptor');
     });
     var scope;
     var timeout;
