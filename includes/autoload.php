4ce4aabd6fbd83edcbd12e3ab84023486ac4d3c0
gabrielmacus <gabrielmacus@gmail.com>
2017-04-27 23:56:14 -0300

    secciones

--- includes/autoload.php
+++ includes/autoload.php
@@ -18,7 +18,7 @@ require("classes/schema/DataSource.php");
 require("classes/schema/Configuracion/Configuracion.php");
 
 
-var_dump("A");
+var_dump("dA");
 
 $configuracion = new Configuracion("localhost","c0580153_adhoc","Mapuche17","c0580153_adhoc",
     "uipasd",0.1,"http://footgolf.mapucheonline.com","Adhoc Framework","Gamaware Web Tech",
