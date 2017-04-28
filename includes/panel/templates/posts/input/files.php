d40b97fd4b03ff73aca6d799abec89782075de68
gabrielmacus <gabrielmacus@gmail.com>
2017-04-27 22:33:30 -0300

    secciones

--- includes/panel/templates/posts/input/files.php
+++ includes/panel/templates/posts/input/files.php
@@ -7,12 +7,13 @@
             scope.previews.splice(idx,1);
             scope.$apply();
         };
+        scope.getMb=function (a,b) {
+
+            if(0==a)return"0 Bytes";var c=1e3,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f];
+
+        }
     });
-    scope.getMb=function (a,b) {
 
-        if(0==a)return"0 Bytes";var c=1e3,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f];
-
-    }
     $(document).on("change","#<?php echo $id?>",function (e) {
 
 
