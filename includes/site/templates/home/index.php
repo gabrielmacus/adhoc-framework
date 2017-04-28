c905496379a4500c22bb00542a13e94d7c267f08
gabrielmacus <gabrielmacus@gmail.com>
2017-04-28 01:47:23 -0300

    secciones

--- includes/site/templates/home/index.php
+++ includes/site/templates/home/index.php
@@ -110,20 +110,7 @@ $(document).ready(function () {
 });
 </script>
 
-<div class="sk-circle">
-    <div class="sk-circle1 sk-child"></div>
-    <div class="sk-circle2 sk-child"></div>
-    <div class="sk-circle3 sk-child"></div>
-    <div class="sk-circle4 sk-child"></div>
-    <div class="sk-circle5 sk-child"></div>
-    <div class="sk-circle6 sk-child"></div>
-    <div class="sk-circle7 sk-child"></div>
-    <div class="sk-circle8 sk-child"></div>
-    <div class="sk-circle9 sk-child"></div>
-    <div class="sk-circle10 sk-child"></div>
-    <div class="sk-circle11 sk-child"></div>
-    <div class="sk-circle12 sk-child"></div>
-</div>
+
 <style>.sk-circle {
         margin: 100px auto;
         width: 40px;
@@ -244,4 +231,33 @@ $(document).ready(function () {
               -webkit-transform: scale(1);
               transform: scale(1);
           }
-    }</style>
+    }
+
+    .loader
+    {
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100%;
+        z-index: 20000;
+        background-color: rgba(16,16,18,0.39);
+    }
+</style>
+
+<div class="loader">
+    <div class="sk-circle">
+        <div class="sk-circle1 sk-child"></div>
+        <div class="sk-circle2 sk-child"></div>
+        <div class="sk-circle3 sk-child"></div>
+        <div class="sk-circle4 sk-child"></div>
+        <div class="sk-circle5 sk-child"></div>
+        <div class="sk-circle6 sk-child"></div>
+        <div class="sk-circle7 sk-child"></div>
+        <div class="sk-circle8 sk-child"></div>
+        <div class="sk-circle9 sk-child"></div>
+        <div class="sk-circle10 sk-child"></div>
+        <div class="sk-circle11 sk-child"></div>
+        <div class="sk-circle12 sk-child"></div>
+    </div>
+</div>
