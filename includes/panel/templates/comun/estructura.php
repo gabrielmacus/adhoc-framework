271818ed2ce03f75a8faf0bc0ecd8780f401d9e9
gabrielmacus <gabrielmacus@gmail.com>
2017-04-29 13:47:10 -0300

    sidenav

--- includes/panel/templates/comun/estructura.php
+++ includes/panel/templates/comun/estructura.php
@@ -16,6 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body class="fila" data-ng-app="panel" data-ng-controller="panelCtrl">
+<?php include DIR_PATH."/includes/panel/templates/comun/submenu.php"?>
 <script>
 
 
--- includes/panel/templates/comun/js.php
+++ includes/panel/templates/comun/js.php
@@ -11,8 +11,8 @@
 <script src="<?php  echo $configuracion->getJsCdn()."/js/vex.combined.min.js"?>"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-animate.js"></script>
 <script  src="<?php  echo $configuracion->getJsCdn()."/js/tether.min.js"?>"></script>
-
 <script  src="<?php  echo $configuracion->getJsCdn()."/js/select.min.js"?>"></script>
+<script  src="<?php  echo $configuracion->getJsCdn()."/js/submenu.js"?>"></script>
 
 <script src="//cdn.quilljs.com/1.2.3/quill.min.js"></script>
 <script
