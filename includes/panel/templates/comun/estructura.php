5d733d4fe6b278e2a82b14569bda5dc904377ea0
gabrielmacus <gabrielmacus@gmail.com>
2017-04-29 13:54:13 -0300

    sidenav

--- includes/panel/templates/comun/estructura.php
+++ includes/panel/templates/comun/estructura.php
@@ -17,55 +17,6 @@
 </head>
 <body class="fila" data-ng-app="panel" data-ng-controller="panelCtrl">
 
-<header class="primary-header">
-    <div class="container">
-        <h1 class="primary-header__title">
-            My Tasks App <small>using custom context menus</small>
-        </h1>
-    </div>
-</header>
-<main class="content">
-    <div class="container">
-        <ul class="tasks">
-            <li class="task" data-id="3">
-                <div class="task__content">
-                    Go To Grocery
-                </div>
-                <div class="task__actions">
-                    <i class="fa fa-eye"></i>
-                    <i class="fa fa-edit"></i>
-                    <i class="fa fa-times"></i>
-                </div>
-            </li>
-            <li class="task" data-id="2">
-                <div class="task__content">
-                    Type Some Code
-                </div>
-                <div class="task__actions">
-                    <i class="fa fa-eye"></i>
-                    <i class="fa fa-edit"></i>
-                    <i class="fa fa-times"></i>
-                </div>
-            </li>
-            <li class="task" data-id="1">
-                <div class="task__content">
-                    Build An App
-                </div>
-                <div class="task__actions">
-                    <i class="fa fa-eye"></i>
-                    <i class="fa fa-edit"></i>
-                    <i class="fa fa-times"></i>
-                </div>
-            </li>
-        </ul>
-    </div>
-</main>
-<footer class="primary-footer">
-    <div class="container">
-        <small>&copy; 2015 Context Menu Madness! Demo by Nick Salloum. <a href="building-custom-context-menu-javascript" target="_blank">See article </a></small>
-    </div>
-</footer>
-<?php include DIR_PATH."/includes/panel/templates/comun/submenu.php"?>
 <script>
 
 
