f41b52653ac4922f272aec12df3b9c8d2dc1896c
gabrielmacus <gabrielmacus@gmail.com>
2017-04-29 13:25:25 -0300

    sidenav

--- includes/panel/templates/archivos/list.php
+++ includes/panel/templates/archivos/list.php
@@ -109,7 +109,6 @@ include DIR_PATH."/includes/panel/templates/archivos/input/files.php";
         foreach ($archivos as $k=>$archivo)
         {
 
-    echo "<h1>{$k}</h1>";
 
             foreach ($archivo as $grupo)
             {
@@ -120,51 +119,50 @@ include DIR_PATH."/includes/panel/templates/archivos/input/files.php";
 
                     $version= reset($versiones);
 
-                    $ext =$version->getExtension();
-
-    
-                    ?>
 
 
-                    <div class="file-preview s6 m4 l3">
-                        <input style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
-                        <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
-                        <div data-ng-click="deletePreview(p)" class="file">
 
-                            <figure>
-                                <?php
+                    switch ($k)
+                    {
+                        case 0://Estandar
 
-                                switch ($ext)
-                                {
-                                    default:
+                            break;
 
-                                        break;
+                        case 1://Imagen
+                            ?>
 
-                                    case "svg":
-                                    case "jpeg":
-                                    case "bmp":
-                                    case "png":
-                                    case "gif":
-                                    case "jpg":
+                            <div class="file-preview s6 m4 l3">
+                                <input style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
+                                <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
+                                <div data-ng-click="deletePreview(p)" class="file">
 
+                                    <figure>
+                                        <?php
                                         $version= $versiones[$versionPanel];
 
                                         ?>
                                         <img data-ng-src="<?php echo $version->getRealName()?>">
-                                        <?php
-                                        break;
-                                }
-                                ?>
-                            </figure>
 
-                            <span class="name"><?php echo $version->getName()?></span>
-                            <span class="size" ><?php echo  bytesToSize($version->getSize())?></span>
-                        </div>
+                                    </figure>
 
-                    </div>
+                                    <span class="name"><?php echo $version->getName()?></span>
+                                    <span class="size" ><?php echo  bytesToSize($version->getSize())?></span>
+                                </div>
 
+                            </div>
 
-                    <?Php
+                            <?php
+                            break;
+                        case 2://Video
+
+                            break;
+
+                        case 3://Audio
+
+                            break;
+                    }
+    
+
                 }
             }
         }
