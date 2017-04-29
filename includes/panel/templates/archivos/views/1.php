2c458f257ecd22c74e8e852fc102d12fc1fb9736
gabrielmacus <gabrielmacus@gmail.com>
2017-04-29 13:29:15 -0300

    sidenav

--- includes/panel/templates/archivos/views/1.php
+++ includes/panel/templates/archivos/views/1.php
@@ -19,7 +19,25 @@ foreach ($archivos as $k=>$archivo)
             switch ($k)
             {
                 case 0://Estandar
+                    ?>
 
+                    <div class="file-preview s6 m4 l3">
+                        <input style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
+                        <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
+                        <div data-ng-click="deletePreview(p)" class="file">
+
+                            <figure>
+                                <i class="fa fa-file-o" aria-hidden="true"></i>
+
+                            </figure>
+
+                            <span class="name"><?php echo $version->getName()?></span>
+                            <span class="size" ><?php echo  bytesToSize($version->getSize())?></span>
+                        </div>
+
+                    </div>
+
+                    <?php
                     break;
 
                 case 1://Imagen
