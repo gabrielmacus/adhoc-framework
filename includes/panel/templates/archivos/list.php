1363aabcfef2bf4a5989350cd83575f157098a96
gabrielmacus <gabrielmacus@gmail.com>
2017-04-30 00:33:59 -0300

    sidenav

--- includes/panel/templates/archivos/list.php
+++ includes/panel/templates/archivos/list.php
@@ -87,7 +87,7 @@ $id="archivos1";
             $progressBar=".load-mask";
 include DIR_PATH."/includes/panel/templates/archivos/input/files.php";
 
-
+            var_dump($paginador->getPaginador());
 
 ?>
 <script>
@@ -115,7 +115,7 @@ include DIR_PATH."/includes/panel/templates/archivos/input/files.php";
     <div class="files">
 
         <?php
-        var_dump($paginador->getPaginador());
+
         include DIR_PATH."/includes/panel/templates/archivos/views/1.php";
         ?>
     </div>
