09f90465725547aeeafa3c421bf32b1929f70db7
gabrielmacus <gabrielmacus@gmail.com>
2017-04-30 03:03:48 -0300

    sidenav

--- includes/panel/templates/posts/input/filesadj.php
+++ includes/panel/templates/posts/input/filesadj.php
@@ -1,22 +1,34 @@
 <script>
     angular.element(function () {
+        var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+        var eventer = window[eventMethod];
+        var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
 
+        // Listen to message from child window
+        eventer(messageEvent,function(e) {
+            if(e.origin == location.href)
+            {
+
+                if(!scope.post.archivos)
+                {
+                    scope.post.archivos=[];
+                }
+                $.each(e.data,function (k,v) {
+                    v.archivo_grupo=<?php echo $grupo;?>;
+                    scope.post.archivos.push(v);
+                });
+             scope.$apply();
+            }
+        },false);
     });
 
-    var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-    var eventer = window[eventMethod];
-    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
 
-    // Listen to message from child window
-    eventer(messageEvent,function(e) {
-        console.log(e.data);
-    },false);
 </script>
 <div class="fila">
     <label><?php echo $label;?></label>
 
     <div class="s12 m4 l4" data-ng-repeat="a in archivos" data-ng-if="a.archivo_grupo==<?php echo $grupo;?>">
-        <span class="name">{{a.nombre}}</span>
+        <span class="name">{{a.name}}</span>
     </div>
 
     <a data-lity class="adjuntas" href="<?php echo $configuracion->getSiteAddress()?>/admin/repositorios/?modal=true">Adjuntar archivo</a>
