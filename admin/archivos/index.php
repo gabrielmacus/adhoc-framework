fee7ef97aea2cca265612546e6afdaeb9450cf29
gabrielmacus <gabrielmacus@gmail.com>
2017-04-30 00:26:25 -0300

    sidenav

--- admin/archivos/index.php
+++ admin/archivos/index.php
@@ -24,10 +24,10 @@ try{
     $GLOBALS["archivoDAO"]->setActualPage($p);
 
     $archivos= $GLOBALS["archivoDAO"]->selectArchivoByRepositorioId($_GET["rep"],true,[$versionPanel]);
-    $paginador=$GLOBALS["archivoDAO"];
+    $paginador=$GLOBALS["archivoDAO"]->getPaginador();
 
 
-    var_dump($paginador);
+
     
     $site="archivos";
     $action="list";
