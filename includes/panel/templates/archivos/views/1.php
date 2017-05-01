cd22f150e415ccc6c36ef0bae49dd8c787d9818d
gabrielmacus <gabrielmacus@gmail.com>
2017-05-01 13:52:27 -0300

    sidenav

--- includes/panel/templates/archivos/views/1.php
+++ includes/panel/templates/archivos/views/1.php
@@ -51,7 +51,7 @@ foreach ($archivos as $k=>$archivo)
                         <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
                         <div data-ng-click="deletePreview(p)" class="file">
 
-                            <div class="mask">
+                            <div class="mask animated">
                                 <div class="half" >
                                     <a class="icon">
                                         <i class="fa fa-search " aria-hidden="true"></i>
