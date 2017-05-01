6c65ddbcd7a822ceb34990b688abbae51699d89e
gabrielmacus <gabrielmacus@gmail.com>
2017-05-01 14:06:11 -0300

    sidenav

--- includes/panel/templates/archivos/views/1.php
+++ includes/panel/templates/archivos/views/1.php
@@ -25,7 +25,7 @@ foreach ($archivos as $k=>$archivo)
                     <div class="file-preview s12 m4 l3">
                         <input  data-name="<?php echo $version->getName()?>" data-id="<?php echo $version->getId()?>" style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
                         <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
-                        <div data-ng-click="deletePreview(p)" class="file">
+                        <div class="file">
 
                             <figure style=" text-align: center;padding-top: 11%;">
                                 <i style="font-size: 150px" class="fa fa-file-o" aria-hidden="true"></i>
@@ -49,7 +49,7 @@ foreach ($archivos as $k=>$archivo)
                     <div class="file-preview s12 m4 l3">
                         <input  data-name="<?php echo $version->getName()?>"  data-id="<?php echo $version->getId()?>"  style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
                         <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
-                        <div data-ng-click="deletePreview(p)" class="file">
+                        <div  class="file">
 
                             <div class="mask animated">
                                 <div class="half" >
@@ -88,7 +88,7 @@ foreach ($archivos as $k=>$archivo)
                     <div class="file-preview s12 m4 l3">
                         <input   data-id="<?php echo $version->getId()?>" data-name="<?php echo $version->getName()?>" style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
                         <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
-                        <div data-ng-click="deletePreview(p)" class="file">
+                        <div " class="file">
 
                             <figure>
                                 <?php
