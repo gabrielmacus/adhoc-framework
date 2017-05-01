32842597cbe5b31c4f65e858d7ef5afc5c20a2e5
gabrielmacus <gabrielmacus@gmail.com>
2017-05-01 14:07:34 -0300

    sidenav

--- includes/panel/templates/archivos/views/1.php
+++ includes/panel/templates/archivos/views/1.php
@@ -23,9 +23,9 @@ foreach ($archivos as $k=>$archivo)
                     ?>
 
                     <div class="file-preview s12 m4 l3">
-                        <input  data-name="<?php echo $version->getName()?>" data-id="<?php echo $version->getId()?>" style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
                         <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
                         <div class="file">
+                            <input  data-name="<?php echo $version->getName()?>" data-id="<?php echo $version->getId()?>" style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
 
                             <figure style=" text-align: center;padding-top: 11%;">
                                 <i style="font-size: 150px" class="fa fa-file-o" aria-hidden="true"></i>
@@ -47,9 +47,9 @@ foreach ($archivos as $k=>$archivo)
                     ?>
 
                     <div class="file-preview s12 m4 l3">
-                        <input  data-name="<?php echo $version->getName()?>"  data-id="<?php echo $version->getId()?>"  style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
-                        <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
+                         <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
                         <div  class="file">
+                            <input   data-id="<?php echo $version->getId()?>" data-name="<?php echo $version->getName()?>" style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
 
                             <div class="mask animated">
                                 <div class="half" >
@@ -86,9 +86,9 @@ foreach ($archivos as $k=>$archivo)
 
                     ?>
                     <div class="file-preview s12 m4 l3">
-                        <input   data-id="<?php echo $version->getId()?>" data-name="<?php echo $version->getName()?>" style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
                         <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
-                        <div " class="file">
+                        <div  class="file">
+                            <input   data-id="<?php echo $version->getId()?>" data-name="<?php echo $version->getName()?>" style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
 
                             <figure>
                                 <?php
