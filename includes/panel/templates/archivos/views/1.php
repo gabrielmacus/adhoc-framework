8f34704cfbf2f165be403da770da0134500c0948
gabrielmacus <gabrielmacus@gmail.com>
2017-05-02 00:42:55 -0300

    sidenav

--- includes/panel/templates/archivos/views/1.php
+++ includes/panel/templates/archivos/views/1.php
@@ -114,6 +114,40 @@ foreach ($archivos as $k=>$archivo)
                 case 3://Audio
 
                     break;
+                case 4:
+
+                    ?>
+                    <div class="file-preview s12 m4 l3">
+                        <input  data-name="<?php echo $version->getName()?>"  data-id="<?php echo $version->getId()?>"  style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
+                        <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
+                        <div  class="file">
+
+                            <div class="mask animated">
+                                <div class="half" >
+                                    <a data-lity  href="http://docs.google.com/gview?url=<?php echo  $version->getRealName();?>&embedded=true" class="icon">
+                                        <i class="fa fa-search " aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                                <div class="half"  >
+                                    <a  download href="<?php echo $version->getRealName()?>" class="icon">
+                                        <i class="fa fa-download" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <figure style=" text-align: center;padding-top: 11%;">
+                                <i style="font-size: 150px" class="fa fa-file-o" aria-hidden="true"></i>
+
+                            </figure>
+
+                            <span class="name"><?php echo $version->getName()?></span>
+                            <!--
+                            <span class="size" ><?php echo  bytesToSize($version->getSize())?></span>
+                        -->
+                        </div>
+
+                    </div>
+                    <?Php
+                    break;
             }
 
 
