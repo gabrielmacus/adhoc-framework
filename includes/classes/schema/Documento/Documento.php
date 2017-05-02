96a594a6c3ec136505e5e2eb63586c2fff3d069f
gabrielmacus <gabrielmacus@gmail.com>
2017-05-02 00:38:14 -0300

    sidenav

--- includes/classes/schema/Documento/Documento.php
+++ includes/classes/schema/Documento/Documento.php
@@ -10,7 +10,7 @@ class Documento extends Archivo
 {
     public function __construct($size, $name, $mime, $version = null, $realName = null, $tmpPath = null, $repositorio, $path = null, $creation = null, $modification = null, $id = null, $versionName = null)
     {
-        parent::__construct($size, $name, $mime, $version, $realName, $tmpPath, $repositorio, $path, $creation, $modification, $id, $versionName, 4);
+        parent::__construct($size, $name, $mime, $version, $realName, $tmpPath, $repositorio, $path, $creation, $modification, $id, $versionName,4);
     }
 
 }
