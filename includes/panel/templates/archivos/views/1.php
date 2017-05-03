7a01ec5e4d758432a6366d77a70bcbdd60f636e6
Gabriel <gabrielmacus@gmail.com>
2017-05-03 10:50:43 -0300

    Pendiente: agregar usuarios con respectivos permisos//Revisar cortes de imagen

--- includes/panel/templates/archivos/views/1.php
+++ includes/panel/templates/archivos/views/1.php
@@ -158,7 +158,7 @@ foreach ($archivos as $k=>$archivo)
 if(count($archivos)==0)
 {
     ?>
-    <div class="fila center" style="font-size: 30px;margin-top: 30px">
+    <div class="fila center" style="font-size: 40px;font-weight: 300;margin-top: 30px">
         <h3>No hay archivos en el repositorio</h3>
     </div>
 
