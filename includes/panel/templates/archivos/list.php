11ec4d06851d537e4f68a443bf7099ab21ab758d
Gabriel <gabrielmacus@gmail.com>
2017-05-03 10:54:24 -0300

    Pendiente: agregar usuarios con respectivos permisos//Revisar cortes de imagen

--- includes/panel/templates/archivos/list.php
+++ includes/panel/templates/archivos/list.php
@@ -95,9 +95,9 @@
 
             <div  class="file-preview s6 m4 l3" data-ng-repeat="p in previews" data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'">
                 <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
-                <div  data-ng-click="deletePreview(p)" class="file" style="position: relative" >
+                <div  class="file" style="position: relative;" >
 
-                    <span style="position: absolute;left: 10px;top: 10px;"><i class="fa fa-times" aria-hidden="true"></i></span>
+                    <span  data-ng-click="deletePreview(p)" style="position: absolute;left: 10px;top: 10px;color: rgba(220, 69, 47, 1);font-size: 23px;"><i class="fa fa-times" aria-hidden="true"></i></span>
                     <figure>
                         <img data-ng-src="{{p.url}}">
                     </figure>
