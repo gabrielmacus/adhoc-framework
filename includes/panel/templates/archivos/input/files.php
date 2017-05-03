53d69302885de8f6dc896e511ddb938108770311
Gabriel <gabrielmacus@gmail.com>
2017-05-03 10:51:02 -0300

    Pendiente: agregar usuarios con respectivos permisos//Revisar cortes de imagen

--- includes/panel/templates/archivos/input/files.php
+++ includes/panel/templates/archivos/input/files.php
@@ -149,7 +149,7 @@
     <input multiple   id="<?php echo $id;?>" type="file" accept="<?Php  echo implode(",",$formats)?>"  >
     -->
     <div class="file-upload">
-        <label for="<?php echo $id;?>" class="file-upload__label">Subir archivo/s</label>
+        <label for="<?php echo $id;?>" class="file-upload__label">Subir archivos</label>
         <input multiple   id="<?php echo $id;?>" type="file" accept="<?Php  echo implode(",",$formats)?>"   class="file-upload__input">
     </div>
 
