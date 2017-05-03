0196c26d66037a9307b85179de93458b6929793f
Gabriel <gabrielmacus@gmail.com>
2017-05-03 17:39:50 -0300

    ()

--- includes/panel/templates/posts/input/filesadj.php
+++ includes/panel/templates/posts/input/filesadj.php
@@ -35,8 +35,9 @@
 <style>
     .adjuntos
     {
-        padding: 20px;
+        padding: 5px;
     }
+
     .adjunto-wrapper
     {
         padding: 15px;
@@ -45,6 +46,7 @@
     .adjuntos .adjunto
     {
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.10);
+        padding: 10px;
     }
 </style>
 <div class="fila adjuntos">
