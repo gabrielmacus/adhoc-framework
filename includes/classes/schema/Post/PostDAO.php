86aa59b807e4286ab06a595ca2eb56aebc72d6bd
gabrielmacus <gabrielmacus@gmail.com>
2017-05-06 16:29:11 -0300

    TODO: fix anexos

--- includes/classes/schema/Post/PostDAO.php
+++ includes/classes/schema/Post/PostDAO.php
@@ -142,7 +142,10 @@ post_texto=:post_texto,post_etiquetas=:post_etiquetas,
                 }
                 else
                 {
+                    /** Asocio objetos de manera bidireccional */
                     $values.=" ('{$anexo['post_nexo_id']}','{$p->getId()}','{$anexo['post_anexo_id']}','{$anexo["post_nexo_grupo"]}','{$anexo['post_nexo_orden']}'),";
+
+                    $values.=" ('{$anexo['post_nexo_id']}','{$anexo['post_anexo_id']}','{$p->getId()}','{$anexo["post_nexo_grupo"]}','{$anexo['post_nexo_orden']}'),";
 
                 }
             }
