acf606e6eaab3cbcdda392a05dacaa2998f577bf
Gabriel <gabrielmacus@gmail.com>
2017-05-08 12:03:48 -0300

    ()

--- includes/panel/templates/posts/input/anexos.php
+++ includes/panel/templates/posts/input/anexos.php
@@ -43,13 +43,13 @@
 <style>
     .adjuntos
     {
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.10);
+
         padding: 10px;
     }
 
 
     .adjunto-wrapper
-    {
+    { box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.10);
         padding: 0px;
         margin-bottom: 20px;
     }
