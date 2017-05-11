61824afb303a1fe8474a2a67e30c68ed8236bef4
Gabriel <gabrielmacus@gmail.com>
2017-05-11 13:26:07 -0300

    ()

--- includes/panel/templates/comun/js.php
+++ includes/panel/templates/comun/js.php
@@ -16,7 +16,8 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-animate.js"></script>
 
-<script src="//cdn.quilljs.com/1.2.3/quill.min.js"></script>
+
+<script  src="<?php  echo $configuracion->getJsCdn()."/js/quill.js"?>"></script>
 <script
 
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAmTWqtcbhdnNXdv-Ow60PedAtYwYgbQ5Q&callback=initMap">
