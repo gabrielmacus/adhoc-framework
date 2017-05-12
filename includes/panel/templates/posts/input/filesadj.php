74595ae6f52f9164d7abcf7669515b541c0dbae1
Gabriel <gabrielmacus@gmail.com>
2017-05-12 15:56:13 -0300

    ()

--- includes/panel/templates/posts/input/filesadj.php
+++ includes/panel/templates/posts/input/filesadj.php
@@ -24,12 +24,11 @@
         }
         $.each(scope.post.archivos,function (tipo,versiones) {
 
-
-
             archivos.push({archivo_id:versiones["<?php echo $fileVersion?>"].id,url:versiones["<?php echo $fileVersion?>"].realName,name:versiones["<?php echo $fileVersion?>"].name,archivo_grupo:versiones["<?php echo $fileVersion?>"].grupo});
 
-
         });
+
+        console.log(archivos);
         scope.post.archivos =archivos;
         scope.$apply();
         /**  **/
