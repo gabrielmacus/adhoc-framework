f86f646015c0727bd57b72e79a64c2172a6e0c7d
Gabriel <gabrielmacus@gmail.com>
2017-05-12 16:35:04 -0300

    ()

--- includes/panel/templates/posts/input/filesadj.php
+++ includes/panel/templates/posts/input/filesadj.php
@@ -21,7 +21,37 @@
         {
             scope.post={};
         }
- 
+
+        var archivos=[];
+        if(!scope.post.archivos) {
+            scope.post.archivos = <?php echo json_encode($post->getArchivos())?>;
+        }
+
+        $.each( scope.post.archivos ,function (tipo,versiones) {
+
+
+            archivos.push({archivo_id:versiones["<?php echo $fileVersion?>"].id,url:versiones["<?php echo $fileVersion?>"].realName,name:versiones["<?php echo $fileVersion?>"].name,archivo_grupo:versiones["<?php echo $fileVersion?>"].grupo});
+
+        });
+
+        scope.post.archivos = archivos;
+        scope.$apply();
+            /*
+            if(!scope.post.archivos)
+            {
+                scope.post.archivos=[];
+               var archivos=<?php echo json_encode($post->getArchivos())?>;
+
+        $.each(archivos,function (tipo,versiones) {
+
+
+            scope.post.archivos .push({archivo_id:versiones["<?php echo $fileVersion?>"].id,url:versiones["<?php echo $fileVersion?>"].realName,name:versiones["<?php echo $fileVersion?>"].name,archivo_grupo:versiones["<?php echo $fileVersion?>"].grupo});
+
+        });
+
+
+        scope.$apply();
+        }*/
         /**  **/
         <?php
         }?>
