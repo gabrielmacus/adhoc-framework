cb3a0cab260541aad409717f2c94ce517df79230
gabrielmacus <gabrielmacus@gmail.com>
2017-05-14 21:47:53 -0300

    nexos

--- includes/panel/templates/noticias/save.php
+++ includes/panel/templates/noticias/save.php
@@ -8,6 +8,10 @@
 
     <form data-ng-submit="save()">
         <?php
+        $label="Titulo";
+        $model="titulo";
+
+        include DIR_PATH."/includes/panel/templates/posts/input/text.php";
 
         $label="Galeria de imágenes";
         $grupo=1;
@@ -34,28 +38,6 @@
         $tipo=60;
         $shownText="titulo";
         include DIR_PATH."/includes/panel/templates/posts/input/anexos.php";
-
-        $label="Texto";
-        $id="data";
-        $model="texto";
-        include DIR_PATH."/includes/panel/templates/posts/input/richtext.php";
-
-        include DIR_PATH."/includes/panel/templates/posts/save.php";
-
-        /*
-
-        $model="extra4";
-        $id="map5";
-        $title ="Marque la ubicación del sapo";
-        include DIR_PATH."/includes/panel/templates/posts/input/map-multiple.php";
-*/
-
-        $name="Guardar cambios";
-        include DIR_PATH."/includes/panel/templates/posts/input/submit.php";
-
-
-
-
 
         ?>
 
