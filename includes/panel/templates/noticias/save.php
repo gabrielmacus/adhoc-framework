0f708f01b28f3761cc727c0f6273bb0d18002b0b
gabrielmacus <gabrielmacus@gmail.com>
2017-05-14 21:48:52 -0300

    nexos

--- includes/panel/templates/noticias/save.php
+++ includes/panel/templates/noticias/save.php
@@ -8,21 +8,7 @@
 
     <form data-ng-submit="save()">
         <?php
-        $label="Titulo";
-        $model="titulo";
 
-        include DIR_PATH."/includes/panel/templates/posts/input/text.php";
-
-        $label="Galeria de imágenes";
-        $grupo=1;
-        $formats=[];//TODO proximamente
-        include DIR_PATH."/includes/panel/templates/posts/input/filesadj.php";
-
-
-        $label="Galeria de imágenes 2";
-        $grupo=45;
-        $formats=[];//TODO proximamente
-        include DIR_PATH."/includes/panel/templates/posts/input/filesadj.php";
 
         $label="Jugadores adjuntos";
         $grupo=25;
@@ -53,6 +39,9 @@
         $title ="Marque la ubicación del sapo";
         include DIR_PATH."/includes/panel/templates/posts/input/map-multiple.php";
 */
+
+        $name="Guardar cambios";
+        include DIR_PATH."/includes/panel/templates/posts/input/submit.php";
 
 
 
