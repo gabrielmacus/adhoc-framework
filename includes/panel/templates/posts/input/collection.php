55287e1fb43d73356f9d4e4ab4401cfb6427c863
gabrielmacus <gabrielmacus@gmail.com>
2017-05-15 00:35:43 -0300

    nexos

--- includes/panel/templates/posts/input/collection.php
+++ includes/panel/templates/posts/input/collection.php
@@ -1,7 +1,7 @@
 
 <div class="form-block <?php echo implode(" ",$class);?>">
     <label><?php echo $label?></label>
-   <ul class="list">
+   <ul class="collection">
       <li>A</li>
    </ul>
 </div>
