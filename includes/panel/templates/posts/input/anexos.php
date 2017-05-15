4f33b4e871e4f2174fa089cb4482569ecd25e72f
gabrielmacus <gabrielmacus@gmail.com>
2017-05-14 23:29:57 -0300

    nexos

--- includes/panel/templates/posts/input/anexos.php
+++ includes/panel/templates/posts/input/anexos.php
@@ -155,7 +155,7 @@ if(!$shownText)
                 <img data-ng-src="{{a.url}}" style="height: 100%;width: 100%;object-fit: cover">
             </figure>-->
             <a   data-lity href="<?php echo $configuracion->getSiteAddress()?>/admin/posts/?modal=true&t=<?php echo $tipo?>&s=<?php echo $s?>&act=view&id={{a.post_anexo_id}}"  class="adjunto">
-                <span class="name" style="white-space: nowrap;overflow: hidden;text-overflow: ellipsis" data-ng-bind="getText(a,<?php echo json_encode($shownText)?>)"></span>
+                <span class="name" style="white-space: nowrap;overflow: hidden;text-overflow: ellipsis" data-ng-bind="getText(a,'<?php echo json_encode($shownText)?>)'"></span>
             </a>
         </div>
 
