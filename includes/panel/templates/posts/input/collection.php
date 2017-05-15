dfea928964e7c71cc2a62cd71cc254ed099492ed
Gabriel <gabrielmacus@gmail.com>
2017-05-15 11:30:56 -0300

    ()

--- includes/panel/templates/posts/input/collection.php
+++ includes/panel/templates/posts/input/collection.php
@@ -1,6 +1,6 @@
 <div class="form-block <?php echo implode(" ",$class);?>">
     <label><?php echo $label?></label>
-   <ul class="collection s12 m6 l6 padding">
+   <ul class="collection ">
       <li class="item">A</li>
    </ul>
 </div>
