72704d507e8841ed0722a40fdb982dea0191b64b
gabrielmacus <gabrielmacus@gmail.com>
2017-05-15 00:11:02 -0300

    nexos

--- includes/panel/templates/noticias/save.php
+++ includes/panel/templates/noticias/save.php
@@ -13,15 +13,6 @@
         $model="titulo";
         include DIR_PATH."/includes/panel/templates/posts/input/text.php";
 
-        $model ="bajada";
-        $label="Nivel";
-        $options=array(
-            1=>"Data 1",
-            2 =>"Data 2",
-            3 =>"Data 3"
-
-        );
-        include DIR_PATH."/includes/panel/templates/posts/input/select.php";
 
 
         $label="Galeria de imágenes";
