8c02e3b896030f86c1f39c65090948524789e392
gabrielmacus <gabrielmacus@gmail.com>
2017-05-14 21:46:57 -0300

    nexos

--- includes/panel/templates/noticias/save.php
+++ includes/panel/templates/noticias/save.php
@@ -8,6 +8,22 @@
 
     <form data-ng-submit="save()">
         <?php
+        $label="Titulo";
+        $model="titulo";
+
+        include DIR_PATH."/includes/panel/templates/posts/input/text.php";
+
+
+        $label="Galeria de imágenes";
+        $grupo=1;
+        $formats=[];//TODO proximamente
+        include DIR_PATH."/includes/panel/templates/posts/input/filesadj.php";
+
+
+        $label="Galeria de imágenes 2";
+        $grupo=45;
+        $formats=[];//TODO proximamente
+        include DIR_PATH."/includes/panel/templates/posts/input/filesadj.php";
 
         $label="Jugadores adjuntos";
         $grupo=25;
@@ -15,6 +31,32 @@
         $tipo=62;
         $shownText=["titulo","volanta"];
         include DIR_PATH."/includes/panel/templates/posts/input/anexos.php";
+
+
+        $label="Hoyos adjuntos";
+        $grupo=26;
+        $s="hoyos";
+        $tipo=60;
+        $shownText="titulo";
+        include DIR_PATH."/includes/panel/templates/posts/input/anexos.php";
+
+
+        include DIR_PATH."/includes/panel/templates/posts/save.php";
+
+        /*
+
+        $model="extra4";
+        $id="map5";
+        $title ="Marque la ubicación del sapo";
+        include DIR_PATH."/includes/panel/templates/posts/input/map-multiple.php";
+*/
+
+        $name="Guardar cambios";
+        include DIR_PATH."/includes/panel/templates/posts/input/submit.php";
+
+
+
+
 
         ?>
 
