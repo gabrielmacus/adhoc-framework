2f08901f0fed1111cc198c7a4320cd0b26755a97
gabrielmacus <gabrielmacus@gmail.com>
2017-05-14 21:55:26 -0300

    nexos

--- includes/panel/templates/posts/save.php
+++ includes/panel/templates/posts/save.php
@@ -17,11 +17,8 @@
 
         {
         ?>
-        if(!scope.post)
-        {
+         scope.post = <?php echo json_encode($post);?>;
 
-            scope.post = <?php echo json_encode($post);?>;
-        }
 
 
         /***  cargo adjuntos **/
