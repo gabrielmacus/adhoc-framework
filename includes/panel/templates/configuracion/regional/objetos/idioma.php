39a8f563b36315d21664b4e32437d138955f8871
Gabriel <gabrielmacus@gmail.com>
2017-05-17 14:14:40 -0300

    ()

--- includes/panel/templates/configuracion/regional/objetos/idioma.php
+++ includes/panel/templates/configuracion/regional/objetos/idioma.php
@@ -1,7 +1,7 @@
 <script>
     angular.element(function () {
 
-        scope.idiomas  = <?Php echo $idiomas ?>;
+        scope.idiomas  = <?Php echo json_encode($idiomas) ?>;
 
         scope.$apply();
 
