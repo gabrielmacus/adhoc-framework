651a562cf5753e61cb8806ca53f94db4e352c87e
Gabriel <gabrielmacus@gmail.com>
2017-05-17 17:26:02 -0300

    ()

--- includes/panel/templates/repositorios/list.php
+++ includes/panel/templates/repositorios/list.php
@@ -12,7 +12,7 @@
         }else
         {
             ?>
-        scope.post = <?php echo json_encode(reset($repositorio))?>;
+        scope.post = <?php echo json_encode($repositorio)?>;
       
         $("#tags1").val();
         $("#add-repositorio").hide();
