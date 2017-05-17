9df923dc88468ea512f72a800240dbe274070c89
Gabriel <gabrielmacus@gmail.com>
2017-05-17 13:47:44 -0300

    ()

--- includes/panel/templates/configuracion/regional/objetos/idioma.php
+++ includes/panel/templates/configuracion/regional/objetos/idioma.php
@@ -1,5 +1,10 @@
 <div class="idioma fila">
     <ul class="table">
         <li class="td s12 m3 l2"><a>Español - es </a></li>
+        <li class="td s12 m3 l2"><a>Esperanto - esp </a></li>
+        <li class="td s12 m3 l2"><a>Baghdidasd - es </a></li>
+        <li class="td s12 m3 l2"><a>Español - es </a></li>
+        <li class="td s12 m3 l2"><a>Español - es </a></li>
+        <li class="td s12 m3 l2"><a>Español - es </a></li>
     </ul>
 </div>
