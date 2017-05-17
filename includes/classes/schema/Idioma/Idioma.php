1fbe0df8b6db160850f8e24423009e5fe5f109d4
Gabriel <gabrielmacus@gmail.com>
2017-05-17 14:28:43 -0300

    ()

--- includes/classes/schema/Idioma/Idioma.php
+++ includes/classes/schema/Idioma/Idioma.php
@@ -12,7 +12,7 @@ class Idioma implements JsonSerializable
     protected $id;
     protected $nombre;
     protected $short;
-    protected $predeterminado=false;
+    protected $predeterminado=0;
 
     function __construct()
     {
@@ -34,7 +34,7 @@ class Idioma implements JsonSerializable
         $this->predeterminado = $predeterminado;
     }
 
-    
+
 
     /**
      * @return mixed
