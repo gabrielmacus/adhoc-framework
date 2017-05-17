f6e56d8e2619fb2068474a66d127749035a662f6
Gabriel <gabrielmacus@gmail.com>
2017-05-17 13:56:15 -0300

    ()

--- includes/panel/templates/noticias/save.php
+++ includes/panel/templates/noticias/save.php
@@ -7,6 +7,23 @@
 <div class="body">
 
     <form data-ng-submit="save()">
+        <?php
+
+        $label="Titulo";
+        $model="titulo";
+        include DIR_PATH."/includes/panel/templates/posts/input/text.php";
+
+
+        $model ="bajada";
+        $label="Nivel";
+        $options=array(
+            1=>"Data 1",
+            2 =>"Data 2",
+            3 =>"Data 3"
+
+        );
+        include DIR_PATH."/includes/panel/templates/posts/input/select.php";
+        ?>
 
     </form>
 
