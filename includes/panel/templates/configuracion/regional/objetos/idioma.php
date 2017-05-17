27b796b9b8dbc0eb7b79dac2e4c6eccb413394c8
Gabriel <gabrielmacus@gmail.com>
2017-05-17 14:33:19 -0300

    ()

--- includes/panel/templates/configuracion/regional/objetos/idioma.php
+++ includes/panel/templates/configuracion/regional/objetos/idioma.php
@@ -11,7 +11,13 @@
 <div class="idioma fila">
     <h3 class="title">Idiomas del sitio</h3>
     <ul class="table">
-        <li data-ng-repeat="i in idiomas" ><a class="td s12 m3 l3 animated"><span class="name">{{i.name}}</span> - <span class="short">{{i.short}}</span> </a></li>
+        <li data-ng-repeat="i in idiomas" ><a class="td s12 m3 l3 animated">
+                <i class="fa fa-star" aria-hidden="true"></i>
+
+                <span class="name">
+                    {{i.name}}</span> - <span class="short">{{i.short}}</span>
+            </a>
+        </li>
     </ul>
 
 </div>
