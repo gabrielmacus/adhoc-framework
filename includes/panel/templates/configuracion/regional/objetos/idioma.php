98611257f72d8c94071c57c733440ca97aeb8ef8
Gabriel <gabrielmacus@gmail.com>
2017-05-17 13:50:20 -0300

    ()

--- includes/panel/templates/configuracion/regional/objetos/idioma.php
+++ includes/panel/templates/configuracion/regional/objetos/idioma.php
@@ -1,5 +1,5 @@
 <div class="idioma fila">
     <ul class="table">
-        <li class="td s12 m3 l3"><a><span class="name">Español</span> - <span class="short">es</span> </a></li>
+        <li ><a class="td s12 m3 l3 animated"><span class="name">Español</span> - <span class="short">es</span> </a></li>
     </ul>
 </div>
