70f519c928e2991d1479cf2139c9875f762dac3b
Gabriel <gabrielmacus@gmail.com>
2017-05-17 13:50:49 -0300

    ()

--- includes/panel/templates/configuracion/regional/objetos/idioma.php
+++ includes/panel/templates/configuracion/regional/objetos/idioma.php
@@ -1,4 +1,5 @@
 <div class="idioma fila">
+    <h3>Idiomas del sitio</h3>
     <ul class="table">
         <li ><a class="td s12 m3 l3 animated"><span class="name">Español</span> - <span class="short">es</span> </a></li>
     </ul>
