8056fc719b2ca5569d1322f7376cb38c781320ce
Gabriel <gabrielmacus@gmail.com>
2017-05-18 12:35:04 -0300

    ()

--- includes/panel/templates/comun/js.php
+++ includes/panel/templates/comun/js.php
@@ -11,7 +11,7 @@
 <script  src="<?php  echo $configuracion->getJsCdn()."/js/taggle.js"?>"></script>
 <script src="<?php  echo $configuracion->getJsCdn()."/js/vex.combined.min.js"?>"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-animate.js"></script>
-<script  src="<?php  echo $configuracion->getJsCdn()."/js/select.min.js"?>"></script>
+<!--<script  src="<?php  echo $configuracion->getJsCdn()."/js/select.min.js"?>"></script>-->
 <script  src="<?php  echo $configuracion->getJsCdn()."/js/contextMenu.min.js"?>"></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-animate.js"></script>
