7e45ec9e8e74cb014fef6a7d195fb30cfac9e25f
gabrielmacus <gabrielmacus@gmail.com>
2017-05-18 00:42:43 -0300

    ()

--- includes/panel/templates/configuracion/secciones/list.php
+++ includes/panel/templates/configuracion/secciones/list.php
@@ -42,19 +42,18 @@ scope.addSubseccion=function (tipo) {
                                 $.ajax
                                 (
                                     {
-                                        method:"get",
-                                        url:"<?php echo $configuracion->getSiteAddress()?>/admin/configuracion/secciones/data.php?act=list",
-                                        dataType:"json",
-                                        success:function (e) {
+                                        method: "get",
+                                        url: "<?php echo $configuracion->getSiteAddress()?>/admin/configuracion/secciones/data.php?act=list",
+                                        dataType: "json",
+                                        success: function (e) {
 
                                             console.log(e);
                                             scope.secciones = e;
                                         }
-                                )
+                                    }
+                                );
 
-
-
-                            toastr.success('', 'Idioma guardado con éxito');
+                            toastr.success('', 'Seccion guardada con éxito');
 
                         },
                         error:error
