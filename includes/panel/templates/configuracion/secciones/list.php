70800ca03c03cdf34b05a0b627cee115febd57e2
Gabriel <gabrielmacus@gmail.com>
2017-05-19 11:11:23 -0300

    ()

--- includes/panel/templates/configuracion/secciones/list.php
+++ includes/panel/templates/configuracion/secciones/list.php
@@ -165,11 +165,12 @@ function loadSecciones() {
     <h2><?php echo $lang["secciones"]?></h2>
 </header>
 <script type="text/ng-template" id="categoryTree">
+    <div class="seccion">
     <span>{{ seccion.nombre }}</span>
     <i data-ng-click="addSubseccion(seccion.id)" class="fa fa-plus-square-o icon add-seccion" aria-hidden="true"></i>
     <i data-ng-click="deleteSeccion(seccion)" data-ng-hide="checkPostsInside(seccion) || seccion.cantPosts > 0" class="fa fa-trash icon" aria-hidden="true"></i>
     <i class="fa fa-pencil icon" aria-hidden="true"></i>
-
+    </div>
     <ul data-ng-if="seccion.secciones">
         <li data-posts="{{seccion.cantPosts}}" data-ng-repeat="(key,seccion)  in seccion.secciones" data-ng-include="'categoryTree'">
 
