dda7e5eca3969b5ebf0cb7c107650407c1526d6e
gabrielmacus <gabrielmacus@gmail.com>
2017-05-20 21:53:37 -0300

    ()

---
+++ includes/panel/templates/gui/input/secciones.php
@@ -0,0 +1,22 @@
+
+<?php
+$secciones = $_POST["seccionDAO"]->selectSeccionesSubsecciones();
+
+
+var_dump($secciones);
+
+?>
+<script>
+    angular.element(function () {
+
+
+
+
+    });
+</script>
+<div title="Secciones"  class="form-block <?php echo implode(" ",$class);?>">
+    <label>Sección</label>
+    <select  data-ng-options="item for item in <?php echo $model?>"
+             data-ng-model="post.<?php echo $model;?>">
+    </select>
+</div>
