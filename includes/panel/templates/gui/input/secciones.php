0260c69a199fd8eec9578247684332e57289d341
gabrielmacus <gabrielmacus@gmail.com>
2017-05-21 15:13:19 -0300

    ()

--- includes/panel/templates/gui/input/secciones.php
+++ includes/panel/templates/gui/input/secciones.php
@@ -5,19 +5,18 @@
 
         scope.secciones = <?php  echo  json_encode($subsecciones);?>;
 
+        scope.loadSecciones = function (s) {
+
+            console.log(s);
+        }
     });
 </script>
 <div title="Secciones"  class="form-block <?php echo implode(" ",$class);?>">
     <label>Sección</label>
-    <select>
+    <select data-ng-change="loadSecciones(s)">
         <option value="">-</option>
         <option  data-ng-repeat="s in secciones" value="{{s.id}}">{{s.nombre}}</option>
     </select>
 
-    <label>Sección</label>
-    <select>
-        <option value="">-</option>
-        <option  data-ng-repeat="s in secciones" value="{{s.id}}">{{s.nombre}}</option>
-    </select>
 
 </div>
