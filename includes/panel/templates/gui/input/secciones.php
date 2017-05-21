76694f4a620d041da5c5611c4a1193597b2186ae
gabrielmacus <gabrielmacus@gmail.com>
2017-05-20 22:03:15 -0300

    ()

--- includes/panel/templates/gui/input/secciones.php
+++ includes/panel/templates/gui/input/secciones.php
@@ -10,6 +10,5 @@
 <div title="Secciones"  class="form-block <?php echo implode(" ",$class);?>">
     <label>Sección</label>
     <select  data-ng-options="item for item in secciones">
-        <option value="{{item.id}}">{{item.nombre}}</option>
     </select>
 </div>
