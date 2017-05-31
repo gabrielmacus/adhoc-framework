a2e8681186a4f7b9b33d9289fd0a0728aef3d777
Gabriel <gabrielmacus@gmail.com>
2017-05-31 10:56:58 -0300

    ()

--- includes/panel/templates/configuracion/menu/list.php
+++ includes/panel/templates/configuracion/menu/list.php
@@ -37,12 +37,12 @@
 <script type="text/ng-template" id="nodes_renderer.html">
     <div class="seccion" ui-tree-handle>
         <span>{{v.text}}</span>
-        <i data-ng-if="seccion.submenu"   data-ng-click="addSubseccion(seccion.id)" class="fa fa-plus-square-o icon add-seccion" aria-hidden="true"></i>
+        <i data-ng-if="seccion.submenu"    data-ng-click="addSubseccion(seccion.id)" class="fa fa-plus-square-o icon add-seccion" aria-hidden="true"></i>
         <i  data-ng-if="seccion.delete"  data-ng-click="deleteSeccion(seccion)" data-ng-hide="checkPostsInside(seccion) || seccion.cantPosts > 0" class="fa fa-trash icon" aria-hidden="true"></i>
         <i data-ng-if="seccion.edit" data-ng-click="editSeccion(seccion)" class="fa fa-pencil icon" aria-hidden="true"></i>
 
     </div>
-    <ul ui-tree-nodes="" ng-model="v.items">
+    <ul data-nodrop-enabled="!seccion.submenu" ui-tree-nodes="" ng-model="v.items">
         <li ng-repeat="(k,v) in v.items" ui-tree-node ng-include="'nodes_renderer.html'">
         </li>
     </ul>
