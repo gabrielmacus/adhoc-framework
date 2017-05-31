480c974885c5b67ae0ec6959ccc300ef2763d5d0
Gabriel <gabrielmacus@gmail.com>
2017-05-31 11:36:44 -0300

    ()

--- includes/panel/templates/configuracion/menu/list.php
+++ includes/panel/templates/configuracion/menu/list.php
@@ -2,6 +2,33 @@
     angular.element(function () {
 
         scope.data=<?php echo json_encode($menu)?>;
+scope.addSubitem=function (v) {
+    vex.dialog.open({
+        message: 'Agregando item de menú',
+        input: [
+            '<label  for="menu-name">Nombre</label>',
+            '<input id="menu-name"  name="name" type="text" required />',
+            '<label>Url (opcional)</label>',
+            '<input  name="url" type="text"  />',
+            '<label><input type="checkbox" name="submenu" checked> Permite submenú</label>'
+
+        ].join(''),
+        buttons: [
+            $.extend({}, vex.dialog.buttons.YES, { text: 'Ok' }),
+            $.extend({}, vex.dialog.buttons.NO, { text: 'Cancelar' })
+        ],
+        callback: function (data) {
+
+            if(data)
+            {
+
+            }
+        }
+
+    });
+
+
+}
 
         scope.save=function () {
 
@@ -37,7 +64,7 @@
 <script type="text/ng-template" id="nodes_renderer.html">
     <div class="seccion" ui-tree-handle>
         <span>{{v.text}}</span>
-        <i data-ng-if="v.submenu"    data-ng-click="addSubseccion(seccion.id)" class="fa fa-plus-square-o icon add-seccion" aria-hidden="true"></i>
+        <i data-ng-if="v.submenu"    data-ng-click="addSubitem(v)" class="fa fa-plus-square-o icon add-seccion" aria-hidden="true"></i>
         <i  data-ng-if="v.delete"  data-ng-click="deleteSeccion(seccion)" data-ng-hide="checkPostsInside(seccion) || seccion.cantPosts > 0" class="fa fa-trash icon" aria-hidden="true"></i>
         <i data-ng-if="v.edit" data-ng-click="editSeccion(seccion)" class="fa fa-pencil icon" aria-hidden="true"></i>
 
