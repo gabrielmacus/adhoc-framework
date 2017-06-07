cac909594ee125b2a377d46ad325fe54dde20ebd
Gabriel <gabrielmacus@gmail.com>
2017-06-07 12:14:39 -0300

    ()

--- includes/site/templates/home/index.php
+++ includes/site/templates/home/index.php
@@ -4,5 +4,16 @@
     ?>
 </div>
 <div class="body">
-    
+
+    <div class="col-1">
+        
+    </div>
+    <div class="col-2">
+
+        <aside class="banner-1">
+            <img src="https://tpc.googlesyndication.com/simgad/11545181438776527248">
+        </aside>
+
+    </div>
+
 </div>
