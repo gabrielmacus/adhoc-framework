8549094ef2f6f9225a06b7196db075ace9349a61
Gabriel <gabrielmacus@gmail.com>
2017-06-07 10:02:21 -0300

    ()

--- includes/site/templates/home/objetos/slider.php
+++ includes/site/templates/home/objetos/slider.php
@@ -3,23 +3,33 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.min.js"></script>
 
+<div class="slider-container">
 
-<!-- Swiper -->
-<div class="swiper-container">
-    <div class="swiper-wrapper">
-        <div class="swiper-slide">
-            <figure>
-                <img src="http://www.markgray.com.au/images/gallery/large/desert-light.jpg">
-            </figure>
-        </div>
-        <div class="swiper-slide">
-            <figure>
-                <img src="http://digital-photography-school.com/wp-content/uploads/flickr/205125227_3f160763a0_o.jpg">
-            </figure>
-        </div>
+    <!-- Swiper -->
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <figure>
+                    <img src="http://www.markgray.com.au/images/gallery/large/desert-light.jpg">
+                </figure>
+            </div>
+            <div class="swiper-slide">
+                <figure>
+                    <img src="http://digital-photography-school.com/wp-content/uploads/flickr/205125227_3f160763a0_o.jpg">
+                </figure>
+            </div>
 
+        </div>
     </div>
+
+    <div class="slider-bar">
+
+        
+        
+    </div>
+
 </div>
+
 
 <!-- Swiper JS -->
 <script src="../dist/js/swiper.min.js"></script>
