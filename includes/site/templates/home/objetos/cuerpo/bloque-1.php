befa3e7d031cb972c03dcfe862d1912ae7b77169
Gabriel <gabrielmacus@gmail.com>
2017-06-07 14:25:20 -0300

    ()

--- includes/site/templates/home/objetos/cuerpo/bloque-1.php
+++ includes/site/templates/home/objetos/cuerpo/bloque-1.php
@@ -1,12 +1,14 @@
-<?php
-for ($i=0;$i<9;$i++)
-{
-    ?>
-    <article>
-        <figure>
-            <img src="http://elonce-media.elonce.com/fotos-nuevo/2017/06/07/b_1496845430.jpg">
-        </figure>
-    </article>
+<div class="bloque-1">
     <?php
-}
-?>
+    for ($i=0;$i<9;$i++)
+    {
+        ?>
+        <article class="item">
+            <figure>
+                <img src="http://elonce-media.elonce.com/fotos-nuevo/2017/06/07/b_1496845430.jpg">
+            </figure>
+        </article>
+        <?php
+    }
+    ?>
+</div>
