c3d78c160c8e9fc7ddcaa254f4bd6e74574c5529
Gabriel <gabrielmacus@gmail.com>
2017-06-07 14:48:22 -0300

    ()

--- includes/site/templates/home/index.php
+++ includes/site/templates/home/index.php
@@ -3,13 +3,16 @@
     include "objetos/header.php";
     ?>
 </div>
-<div class="body">
 
-    <div class="col-1">
+<div class="body-container-1">
+    <div class="body">
 
-        <?php include "objetos/cuerpo/bloque-1.php"; ?>
-        
+        <div class="col-1">
+
+            <?php include "objetos/cuerpo/bloque-1.php"; ?>
+
+        </div>
+
+
     </div>
-
-
 </div>
