478e3c4c2f3e5f0b20374b145bb3e826e90e7feb
Gabriel <gabrielmacus@gmail.com>
2017-06-07 09:19:22 -0300

    ()

--- includes/site/templates/comun/css.php
+++ includes/site/templates/comun/css.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/estilo.css">
+
 <link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/site.css">
 <link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/lity.min.css">
 <link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/reset.css">
