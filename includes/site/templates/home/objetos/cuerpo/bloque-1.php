2171fc58ec316a54de5d1ffb36a53727f17c6471
Gabriel <gabrielmacus@gmail.com>
2017-06-07 14:58:45 -0300

    ()

--- includes/site/templates/home/objetos/cuerpo/bloque-1.php
+++ includes/site/templates/home/objetos/cuerpo/bloque-1.php
@@ -6,10 +6,10 @@
         ?>
        <div class="item-container">
            <article class="item">
-               <figure>
+               <div class="image">
                    <img src="<?php echo $arr[rand(0,count($arr)-1)];?>">
 
-               </figure>
+               </div>
                <h3 class="title">
                    <span class="text">Cuenta regresiva para Tecnópolis Federal: capacitaron a los guías</span>
                </h3>
