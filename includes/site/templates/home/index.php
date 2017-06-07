35c6ee5e1ca8a22f60b500d4c4a6977cdc8e28a6
Gabriel <gabrielmacus@gmail.com>
2017-06-07 12:53:35 -0300

    ()

--- includes/site/templates/home/index.php
+++ includes/site/templates/home/index.php
@@ -1,10 +1,10 @@
-
+<div class="top">
+    <?PHP
+    include "objetos/header.php";
+    ?>
+</div>
 <div class="body">
-    <div class="top">
-        <?PHP
-        include "objetos/header.php";
-        ?>
-    </div>
+
     <div class="col-1">
 
     </div>
