189c07362a96de8e6d3b86f6945f43e1670c9561
Gabriel <gabrielmacus@gmail.com>
2017-06-07 14:35:37 -0300

    ()

--- includes/site/templates/home/objetos/cuerpo/bloque-1.php
+++ includes/site/templates/home/objetos/cuerpo/bloque-1.php
@@ -1,12 +1,12 @@
 <div class="bloque-1">
     <?php
-    $arr =["http://elonce-media.elonce.com/fotos-nuevo/2017/05/29/s_1496079365.jpg","http://elonce-media.elonce.com/fotos-nuevo/2017/06/07/b_1496845430.jpg"];
-    for ($i=0;$i<9;$i++)
+    $arr =["http://elonce-media.elonce.com/fotos-nuevo/2015/10/22/s_1445526983.jpg","http://elonce-media.elonce.com/fotos-nuevo/2017/05/29/s_1496079365.jpg","http://elonce-media.elonce.com/fotos-nuevo/2017/06/07/b_1496845430.jpg"];
+    for ($i=0;$i<6;$i++)
     {
         ?>
         <article class="item">
             <figure>
-                <img src="<?php echo $arr[rand(0,1)];?>">
+                <img src="<?php echo $arr[rand(0,count($arr)-1)];?>">
             </figure>
         </article>
         <?php
