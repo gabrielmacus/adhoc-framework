de1e490c78afc6921381831c780fcb7499a6cc96
Gabriel <gabrielmacus@gmail.com>
2017-06-07 09:47:29 -0300

    ()

--- includes/site/templates/home/objetos/slider.php
+++ includes/site/templates/home/objetos/slider.php
@@ -7,16 +7,12 @@
 <!-- Swiper -->
 <div class="swiper-container">
     <div class="swiper-wrapper">
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 4</div>
-        <div class="swiper-slide">Slide 5</div>
-        <div class="swiper-slide">Slide 6</div>
-        <div class="swiper-slide">Slide 7</div>
-        <div class="swiper-slide">Slide 8</div>
-        <div class="swiper-slide">Slide 9</div>
-        <div class="swiper-slide">Slide 10</div>
+        <div class="swiper-slide">
+            <figure>
+                <img src="http://www.markgray.com.au/images/gallery/large/desert-light.jpg">
+            </figure>
+
+        </div>
     </div>
 </div>
 
