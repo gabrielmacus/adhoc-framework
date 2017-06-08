11393b4a9fbb502183da5cfdda65b6bb3227a83f
Gabriel <gabrielmacus@gmail.com>
2017-06-08 12:39:59 -0300

    ()

--- includes/panel/templates/archivos/views/1.php
+++ includes/panel/templates/archivos/views/1.php
@@ -54,7 +54,7 @@ foreach ($archivos as $k=>$archivo)
 
                 case 1://Imagen
 
-                    var_dump($vp);
+                    var_dump($versiones[$vp]);
                     $version= $versiones[$vp];
 
                     ?>
