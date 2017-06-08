dcdf6df1a0828584815e244799b673c738ba323f
Gabriel <gabrielmacus@gmail.com>
2017-06-08 12:36:27 -0300

    ()

--- includes/panel/templates/archivos/views/1.php
+++ includes/panel/templates/archivos/views/1.php
@@ -31,29 +31,7 @@ foreach ($archivos as $k=>$archivo)
 
             switch ($k)
             {
-                case 0://Estandar
-                    ?>
-
-                    <div class="file-preview s12 m4 l3">
-                        <input data-url="<?Php echo $version->getRealName();?>" data-name="<?php echo $version->getName()?>" data-id="<?php echo $versiones["original"]->getId()?>" style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
-                        <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
-                        <div class="file">
-
-                            <figure style=" text-align: center;padding-top: 11%;">
-                                <i style="font-size: 150px" class="fa fa-file-o" aria-hidden="true"></i>
-
-                            </figure>
-
-                            <span class="name"><?php echo $version->getName()?></span>
-
-                        <!--    <span class="size" ><?php echo  bytesToSize($version->getSize())?></span>
-                        -->
-                        </div>
-
-                    </div>
-
-                    <?php
-                    break;
+              
 
                 case 1://Imagen
                     ?>
