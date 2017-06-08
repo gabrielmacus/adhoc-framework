6e022440143e8f88c1af5f15dd8a63e66e05ad07
Gabriel <gabrielmacus@gmail.com>
2017-06-08 12:25:59 -0300

    ()

--- includes/classes/schema/Archivo/ArchivoDAO.php
+++ includes/classes/schema/Archivo/ArchivoDAO.php
@@ -232,6 +232,8 @@ archivo_id=:archivo_id, archivo_size=:archivo_size,archivo_mime=:archivo_mime, a
             $sql .= "  LIMIT {$this->getLimit()} OFFSET {$offset}";
         }
 
+        var_dump($sql);
+
         $originales = $this->dataSource->runQuery($sql);
 
 
