5b0900e09382101af1eedf86ee2375dad62d1b12
Gabriel <gabrielmacus@gmail.com>
2017-06-08 12:38:14 -0300

    ()

--- includes/panel/templates/archivos/views/1.php
+++ includes/panel/templates/archivos/views/1.php
@@ -26,9 +26,6 @@ foreach ($archivos as $k=>$archivo)
             }
 
 
-            var_dump($vp);
-
-
             switch ($k)
             {
                 case 0://Estandar
@@ -58,7 +55,34 @@ foreach ($archivos as $k=>$archivo)
                 case 1://Imagen
                     ?>
 
-                  
+                    <div class="file-preview s12 m4 l3">
+                        <input data-url="<?Php echo $version->getRealName();?>" data-name="<?php echo $version->getName()?>"   data-id="<?php echo $versiones["original"]->getId()?>" style="position: absolute;top: 20px;left: 20px;-webkit-transform: scale(1.7);-moz-transform: scale(1.7);-ms-transform: scale(1.7);-o-transform: scale(1.7);transform: scale(1.7);" type="checkbox">
+                        <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
+                        <div  class="file">
+
+                            <div class="mask animated">
+                                <div class="half" >
+                                    <a data-lity  href="<?php echo $version->getRealName()?>" class="icon">
+                                        <i class="fa fa-search " aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                                <div class="half"  >
+                                    <a  download href="<?php echo $version->getRealName()?>" class="icon">
+                                        <i class="fa fa-download" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <figure>
+                            </figure>
+
+                            <span class="name"><?php echo $version->getName()?></span>
+                        <!--
+                            <span class="size" ><?php echo  bytesToSize($version->getSize())?></span>
+                        -->
+                        </div>
+
+                    </div>
+
                     <?php
                     break;
                 case 2://Video
