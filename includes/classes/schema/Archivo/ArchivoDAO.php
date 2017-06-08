39721e3808c7f59c5c88ae5b7d6ae6cf8970ef2b
Gabriel <gabrielmacus@gmail.com>
2017-06-08 12:13:12 -0300

    ()

--- includes/classes/schema/Archivo/ArchivoDAO.php
+++ includes/classes/schema/Archivo/ArchivoDAO.php
@@ -283,7 +283,7 @@ archivo_id=:archivo_id, archivo_size=:archivo_size,archivo_mime=:archivo_mime, a
 
           $versiones=  $this->dataSource->runQuery($sql);
 
-            var_dump($versiones);
+            echo json_encode($versiones);
             exit();
 
         }
