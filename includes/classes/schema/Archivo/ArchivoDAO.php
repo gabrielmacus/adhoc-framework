24abc2ae4ff5482fa7c6ad2deee6ff3328a9ede1
Gabriel <gabrielmacus@gmail.com>
2017-06-08 12:30:27 -0300

    ()

--- includes/classes/schema/Archivo/ArchivoDAO.php
+++ includes/classes/schema/Archivo/ArchivoDAO.php
@@ -316,8 +316,7 @@ archivo_id=:archivo_id, archivo_size=:archivo_size,archivo_mime=:archivo_mime, a
         {
             $archivos = $originales;
         }
-   echo json_encode($archivos);
-        exit();
+
         foreach ($archivos as $archivo)
         {
           $this->query($archivo);
