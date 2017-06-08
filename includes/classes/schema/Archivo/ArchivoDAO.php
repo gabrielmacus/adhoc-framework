8e13b36188dae365b4941508547b2a545793114d
Gabriel <gabrielmacus@gmail.com>
2017-06-08 12:24:46 -0300

    ()

--- includes/classes/schema/Archivo/ArchivoDAO.php
+++ includes/classes/schema/Archivo/ArchivoDAO.php
@@ -316,7 +316,10 @@ archivo_id=:archivo_id, archivo_size=:archivo_size,archivo_mime=:archivo_mime, a
         {
           $this->query($archivo);
         }
-echo json_encode($this->files);
+
+        echo json_encode($this->files);
+        exit();
+
         /**
          */
 
