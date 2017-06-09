a38a11a222ac3bcad0062e382b53c6ada0d1ef6e
Gabriel <gabrielmacus@gmail.com>
2017-06-09 09:49:24 -0300

    ()

--- includes/site/templates/home/objetos/cuerpo/bloque-1.php
+++ includes/site/templates/home/objetos/cuerpo/bloque-1.php
@@ -31,7 +31,7 @@
                <span class="tag animated" style="background-color: rgba(<?php echo $lang["secciones"][$data->getSeccion()]["color"];?>,0.25)">
 
 
-                          <span style="background-color: rgba(<?php echo $lang["secciones"][$data->getSeccion()]["color"];?>" class="ribbon">
+                          <span style="background-color: rgba(<?php echo $lang["secciones"][$data->getSeccion()]["color"];?>)" class="ribbon">
                               <?php
 
                               echo  $secciones[$data->getSeccion()]->getNombre();
