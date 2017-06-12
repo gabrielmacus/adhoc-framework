7a7311e76882521596cdf7a51f63b906fb5d5bcf
Gabriel <gabrielmacus@gmail.com>
2017-06-12 11:48:01 -0300

    ()

--- includes/panel/templates/gui/input/anexos.php
+++ includes/panel/templates/gui/input/anexos.php
@@ -212,6 +212,8 @@ if(!$shownText)
 
 <div class="fila">
     <div class="sortable-container" sv-root sv-part="data.items1">
+
+        <span sv-handle>handle</span>
         <div ng-repeat="item in data.items1" sv-element="opts" class="well">
             {{item}}
         </div>
