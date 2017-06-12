1c6e243d9c3adfb5418092153d231e504fb17d34
Gabriel <gabrielmacus@gmail.com>
2017-06-12 12:38:28 -0300

    ()

--- includes/panel/templates/gui/input/anexos.php
+++ includes/panel/templates/gui/input/anexos.php
@@ -206,9 +206,7 @@ if(!$shownText)
         </div>
 
     </div>
+        <a data-lity class="btn" style="color: white!important;" href="/admin/posts/?modal=true&grupo=<?php echo $grupo?>&s=<?php echo $s;?>&t=<?php echo $tipo;?>"><?php echo $textBtn;?></a>
 
-</div>
-<div class="fila" style="margin-top: 15px">
-    <a data-lity class="btn" style="color: white!important;" href="/admin/posts/?modal=true&grupo=<?php echo $grupo?>&s=<?php echo $s;?>&t=<?php echo $tipo;?>"><?php echo $textBtn;?></a>
 
 </div>
