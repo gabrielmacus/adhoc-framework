3a1f95e8ef5aa9e280ebe88ae02672c070333827
Gabriel <gabrielmacus@gmail.com>
2017-06-12 11:27:45 -0300

    ()

--- includes/panel/templates/comun/estructura.php
+++ includes/panel/templates/comun/estructura.php
@@ -26,7 +26,7 @@
     }
 
 
-    var app = angular.module('panel', ['ngAnimate','ui.tree','ngSanitize']);
+    var app = angular.module('panel', ['ngAnimate','ui.tree','ngSanitize','as.sortable']);
     var scope;
     var timeout;
     var http;
