d52f606fae1af37a8b76daadd2ac5e2b95ef52a2
Gabriel <gabrielmacus@gmail.com>
2017-06-13 12:48:48 -0300

    ()

--- includes/classes/schema/Repositorio/RepositorioDAO.php
+++ includes/classes/schema/Repositorio/RepositorioDAO.php
@@ -231,7 +231,7 @@ repositorio_modification=:repositorio_modification,repositorio_url=:repositorio_
         $sql=$this->updateSql;
 
         $r->setModification(time());
-
+var_dump( $r->getVersiones());
         $r->setVersiones(str_replace('\"',"",json_encode( $r->getVersiones())));
 
 
