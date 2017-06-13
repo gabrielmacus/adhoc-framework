6acdc445c481f48a1f5bb3305e374ab722d630aa
Gabriel <gabrielmacus@gmail.com>
2017-06-13 10:24:51 -0300

    ()

--- includes/classes/schema/Archivo/ArchivoDAO.php
+++ includes/classes/schema/Archivo/ArchivoDAO.php
@@ -492,7 +492,7 @@ archivo_id=:archivo_id, archivo_size=:archivo_size,archivo_mime=:archivo_mime, a
         /**
          * Cambiado el 13.6.2017 , revertir si trae problemas
          */
-        $this->files[$a->getId()]=$a;
+        $this->files[$a->getId()][$a->getVersion()]=$a;
       //  array_push($this->files, $a);
 
     }
