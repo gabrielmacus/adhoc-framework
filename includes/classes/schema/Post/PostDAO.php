852880ffd3c680f418d5820fa646b7f8d5487854
Gabriel <gabrielmacus@gmail.com>
2017-06-14 16:14:43 -0300

    ()

--- includes/classes/schema/Post/PostDAO.php
+++ includes/classes/schema/Post/PostDAO.php
@@ -700,8 +700,10 @@ ON posts_filter.post_id = p.post_id";
         /*** **/
 
 
+        $subQuery="SELECT {$fields} FROM {$this->tableName} p {$archivosFilterSql} {$anexosFilterSql} {$where} GROUP BY p.post_id";
 
-        $sql = "SELECT {$fields} FROM {$this->tableName} p {$archivosFilterSql} {$anexosFilterSql} {$where} GROUP BY p.post_id";
+
+        $sql = "SELECT * FROM ({$subQuery}) {$where}";
 
         var_dump($sql);
 
