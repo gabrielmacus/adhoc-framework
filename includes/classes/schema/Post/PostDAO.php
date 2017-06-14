3fbf62e3cd0ab658c8ad3769c4fec0aaec68367c
Gabriel <gabrielmacus@gmail.com>
2017-06-14 09:13:49 -0300

    ()

--- includes/classes/schema/Post/PostDAO.php
+++ includes/classes/schema/Post/PostDAO.php
@@ -385,17 +385,18 @@ post_texto=:post_texto,post_etiquetas=:post_etiquetas,
                 {
                    // $postArchivos[$nexo["archivo_grupo"]][$idOriginal][$archivo->getVersionName()]=$archivo;
                     $postArchivos[$nexo["archivo_grupo"]][$nexo["archivo_orden"]][$archivo->getVersionName()]=$archivo;
-
+                    arsort(  $postArchivos[$nexo["archivo_grupo"]]);
                 }
                 else
                 {
 
                     $postArchivos[$nexo["archivo_orden"]][$archivo->getVersionName()]=$archivo;
 
+                    arsort($postArchivos[$nexo["archivo_orden"]]);
                     //$postArchivos[$idOriginal][$archivo->getVersionName()]=$archivo;
                 }
 
-                 arsort($postArchivos);
+
                 /**
                  *
                  */
