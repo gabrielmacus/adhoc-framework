ec8a8569fa5c81b54a6ec7eaa17c7c855620ee43
Gabriel <gabrielmacus@gmail.com>
2017-06-15 15:22:40 -0300

    ()

--- includes/panel/templates/gui/input/filters.php
+++ includes/panel/templates/gui/input/filters.php
@@ -1,10 +1,10 @@
 <form  class="fila padding" >
     
     <div class="filters">
-        <div class="form-block s12 m6 l6">
+        <div class="form-block s12 m6 l6 search" >
             <label>Buscar por texto</label>
             <input type="text" name="q">
-            <a class="search" ><i class="fa fa-search" aria-hidden="true"></i>
+            <a class="search-action" ><i class="fa fa-search" aria-hidden="true"></i>
             </a>
         </div>
     </div>
