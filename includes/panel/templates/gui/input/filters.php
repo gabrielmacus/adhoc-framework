f6d2692b4ea2c4955b863b7065a3f193975d37d4
Gabriel <gabrielmacus@gmail.com>
2017-06-15 15:38:45 -0300

    ()

--- includes/panel/templates/gui/input/filters.php
+++ includes/panel/templates/gui/input/filters.php
@@ -4,10 +4,10 @@
         <div class="form-block s12 m6 l6 search" >
             <label>Buscar por texto</label>
             <input type="text" name="q">
-            <a class="search-action animated" ><i class="fa fa-search" aria-hidden="true"></i>
+            <a class="search-action " ><i class="fa fa-search" aria-hidden="true"></i>
             </a>
-            <a class="search-plus animated"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
-            <a class="search-minus animated" style="display: none"><i class="fa fa-search-minus" aria-hidden="true"></i></a>
+            <a class="search-plus "><i class="fa fa-search-plus" aria-hidden="true"></i></a>
+            <a class="search-minus " style="display: none"><i class="fa fa-search-minus" aria-hidden="true"></i></a>
         </div>
         <div class="advanced-filters fila" style="display: none">
             <div class="form-block s12 m6 l4">
