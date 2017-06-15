ed614777a5836a6a9f09a8717a8b57383e5d02de
Gabriel <gabrielmacus@gmail.com>
2017-06-15 15:35:45 -0300

    ()

--- includes/panel/templates/gui/input/filters.php
+++ includes/panel/templates/gui/input/filters.php
@@ -6,9 +6,22 @@
             <input type="text" name="q">
             <a class="search-action animated" ><i class="fa fa-search" aria-hidden="true"></i>
             </a>
-            <a class="search-plus"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
-            <a class="search-minus" style="display: none"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
+            <a class="search-plus animated"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
+            <a class="search-minus animated" style="display: none"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
         </div>
     </div>
   
 </form>
+
+<script>
+    $(document).on("click",".search-plus,.search-minus",function () {
+
+
+        $(".search-plus").toggle();
+        $(".search-minus").toggle();
+
+
+    });
+
+
+</script>
