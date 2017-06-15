ecc60fbf8efbe8f431083973363974602fddd937
Gabriel <gabrielmacus@gmail.com>
2017-06-15 15:18:10 -0300

    ()

--- includes/panel/templates/gui/input/filters.php
+++ includes/panel/templates/gui/input/filters.php
@@ -4,6 +4,8 @@
         <div class="form-block s12 m6 l6">
             <label>Buscar por texto</label>
             <input type="text" name="q">
+            <a class="search" ><i class="fa fa-search" aria-hidden="true"></i>
+            </a>
         </div>
     </div>
   
