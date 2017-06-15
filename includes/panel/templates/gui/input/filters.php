adc0f5020cb70d331776f63180385a637ea10788
Gabriel <gabrielmacus@gmail.com>
2017-06-15 15:24:12 -0300

    ()

--- includes/panel/templates/gui/input/filters.php
+++ includes/panel/templates/gui/input/filters.php
@@ -4,7 +4,7 @@
         <div class="form-block s12 m6 l6 search" >
             <label>Buscar por texto</label>
             <input type="text" name="q">
-            <a class="search-action" ><i class="fa fa-search" aria-hidden="true"></i>
+            <a class="search-action animated" ><i class="fa fa-search" aria-hidden="true"></i>
             </a>
         </div>
     </div>
