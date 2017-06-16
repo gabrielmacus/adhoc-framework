a2d1ae703aeaea4bf3c751b423a7beb5c6e0b2de
Gabriel <gabrielmacus@gmail.com>
2017-06-16 09:52:20 -0300

    ()

--- includes/panel/templates/gui/input/add.php
+++ includes/panel/templates/gui/input/add.php
@@ -1,4 +1,6 @@
-<div class="fila" style="margin-top: 15px">
+
+
+<div class="fila fixed-button" style="margin-top: 15px">
 
     <a class="btn" href="<?php echo $href;?>" style="color: white!important;"><?php echo $title;?></a>
 
