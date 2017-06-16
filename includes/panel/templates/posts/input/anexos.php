c39c12c9fb68e53982af01ee2633668d69275af6
Gabriel <gabrielmacus@gmail.com>
2017-06-16 16:08:49 -0300

    ()

--- includes/panel/templates/posts/input/anexos.php
+++ includes/panel/templates/posts/input/anexos.php
@@ -57,9 +57,7 @@ if(!$shownText)
         }
 
 
-        <?php
-        /*
-         *
+     
         var anexos =[];
         $.each(scope.post.anexos,function (k,v) {
 
@@ -96,8 +94,6 @@ if(!$shownText)
 
 
         scope.$apply();
-         * */
-        ?>
 
 
         <?Php
