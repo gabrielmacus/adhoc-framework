587ac3edc0aaf472c1c3491c19236f198df190e9
Gabriel <gabrielmacus@gmail.com>
2017-06-16 13:51:22 -0300

    ()

--- includes/panel/templates/gui/paginador.php
+++ includes/panel/templates/gui/paginador.php
@@ -13,6 +13,10 @@
     );
 </script>
 
+<?php if(!empty($pg))
+
+{
+    ?>
 <div class="fila center">
     <div class="pagination">
         <?php if($actualPage>1)
@@ -21,8 +25,7 @@
             <a class="animated prev"  href="?<?php   $qs=http_build_query($_GET);  echo $qs."&p=".($actualPage-1)?>">&laquo;</a>
             <?php
         }
-        ?>
-        <?php
+
         unset($_GET["p"]);
         $qs=http_build_query($_GET);
         foreach ($pg as $p)
@@ -30,8 +33,9 @@
             ?>
             <a class="animated <?php echo $p["class"]?>" href="?<?php  echo $qs."&p=".$p["number"]?>"><?php echo $p["number"] ?></a>
             <?php
-        }?>
-        <?php if($actualPage<$pages)
+        }
+
+        if($actualPage<$pages)
         {
             ?>
             <a class="animated next" href="?<?php  echo $qs."&p=".($actualPage+1)?>">&raquo;</a>
@@ -39,3 +43,5 @@
         }?>
     </div>
 </div>
+    <?php
+}?>
