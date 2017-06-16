a7f0e61675a7b39780516e749e1c57d243cf6266
Gabriel <gabrielmacus@gmail.com>
2017-06-16 09:59:33 -0300

    ()

--- includes/panel/templates/gui/input/add.php
+++ includes/panel/templates/gui/input/add.php
@@ -10,8 +10,7 @@
         function () {
 
             clearTimeout(timeout);
-            clearTimeout($.data(this, 'scrollTimer'));
-            $.data(this, 'scrollTimer', setTimeout(function() {
+
                 // do something
 
                 $(".fixed-button").css("opacity","0.15");
@@ -20,7 +19,7 @@
                     $(".fixed-button").css("opacity","1");
                     clearTimeout(timeout);
                 },2500)
-            }, 250));
+
 
 
 
