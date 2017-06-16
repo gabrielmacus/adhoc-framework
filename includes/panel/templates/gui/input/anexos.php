255ba382cf37a3e200430fc4c09e414c25c30294
Gabriel <gabrielmacus@gmail.com>
2017-06-16 16:38:50 -0300

    ()

--- includes/panel/templates/gui/input/anexos.php
+++ includes/panel/templates/gui/input/anexos.php
@@ -192,7 +192,7 @@ if(!$shownText)
 
 
     <ul class="grid"  id="sortable<?php echo $grupo;?>" >
-        <li data-idx="{{k}}" class="s12 m6 l4 padding " data-ng-repeat="(k,a) in  post.anexos" data-ng-if="a.post_nexo_grupo==<?php echo $grupo;?>"  data-ng-hide="a.delete">
+        <li class="item" data-idx="{{k}}" class="s12 m6 l4 padding " data-ng-repeat="(k,a) in  post.anexos" data-ng-if="a.post_nexo_grupo==<?php echo $grupo;?>"  data-ng-hide="a.delete">
 
             <div  class="adjunto-wrapper" style="position: relative">
               <span data-ng-click="removeAnexo(a)" style="font-size:30px;cursor: pointer;position: absolute;z-index: 55;top: 5px;right:5px;color: rgba(220, 69, 47, 1)">
@@ -224,18 +224,22 @@ if(!$shownText)
         $( "#sortable<?php echo $grupo;?>" ).sortable({
             revert: true,
             stop: function() {
-             
-                var i=0;
-                
-                $("#sortable<?php echo $grupo;?>").each(
+
+                var anexos=angular.copy(scope.post.anexos);
+               var firstIdx= $("#sortable<?php echo $grupo;?> .item").first().data("idx");
+
+                $("#sortable<?php echo $grupo;?> .item").each(
                     function (k,v) {
 
-                        console.log(k);
+                        var idx = $(this).data("idx");
 
-                        console.log(v);
+                        scope.post.anexos[firstIdx+k]=anexos[idx];
+
 
                     }
                 );
+
+                scope.$apply();
                 
                 
                 
