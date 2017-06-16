1e961f58d10ad3cbc98fb49aa460ee37a4a16b9f
Gabriel <gabrielmacus@gmail.com>
2017-06-16 09:54:40 -0300

    ()

--- includes/panel/templates/gui/input/add.php
+++ includes/panel/templates/gui/input/add.php
@@ -2,6 +2,7 @@
 
 <div class=" fixed-button" >
 
-    <a class="btn" href="<?php echo $href;?>" style="color: white!important;"><?php echo $title;?></a>
+    <a class="btn" href="<?php echo $href;?>" style="color: white!important;"><i class="fa fa-plus" aria-hidden="true"></i>
+    </a>
 
 </div>
