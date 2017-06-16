8c5e77a28a765abeb5e661b9d674f05bb1a0ad2b
Gabriel <gabrielmacus@gmail.com>
2017-06-16 16:12:10 -0300

    ()

--- includes/panel/templates/gui/input/anexos.php
+++ includes/panel/templates/gui/input/anexos.php
@@ -219,3 +219,28 @@ if(!$shownText)
     </div>
 
 </div>
+<script>
+    $( function() {
+        $( "#sortable" ).sortable({
+            revert: true
+        });
+        $( "#draggable" ).draggable({
+            connectToSortable: "#sortable",
+            helper: "clone",
+            revert: "invalid"
+        });
+        $( "ul, li" ).disableSelection();
+    } );
+</script>
+
+<ul>
+    <li id="draggable" class="ui-state-highlight">Drag me down</li>
+</ul>
+
+<ul id="sortable">
+    <li class="ui-state-default">Item 1</li>
+    <li class="ui-state-default">Item 2</li>
+    <li class="ui-state-default">Item 3</li>
+    <li class="ui-state-default">Item 4</li>
+    <li class="ui-state-default">Item 5</li>
+</ul>
