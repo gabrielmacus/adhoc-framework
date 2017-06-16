646e913b3a19f682867bf3dd341cfa6e2aa6195e
Gabriel <gabrielmacus@gmail.com>
2017-06-16 09:56:53 -0300

    ()

--- includes/panel/templates/gui/input/add.php
+++ includes/panel/templates/gui/input/add.php
@@ -5,3 +5,12 @@
     <a class="btn" href="<?php echo $href;?>" style="color: white!important;"><?php echo $title;?></a>
 
 </div>
+<script>
+    $(document).scroll(
+        function () {
+
+            $(".fixed-button").css("opacity","0.3");
+
+        }
+    );
+</script>
