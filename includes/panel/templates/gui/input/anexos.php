5a379c396e351dc7ae6cac565d249fb4cb7ff359
Gabriel <gabrielmacus@gmail.com>
2017-06-16 16:10:59 -0300

    ()

--- includes/panel/templates/gui/input/anexos.php
+++ includes/panel/templates/gui/input/anexos.php
@@ -191,7 +191,7 @@ if(!$shownText)
     <label class="fila" style="margin-bottom: 10px;"><?php echo $label;?></label>
 
 
-    <div   class="s12 m6 l4 padding " data-ng-repeat="a in  post.anexos" data-ng-if="a.post_grupo==<?php echo $grupo;?>"  data-ng-hide="a.delete">
+    <div   class="s12 m6 l4 padding " data-ng-repeat="a in  post.anexos" data-ng-if="a.anexo_grupo==<?php echo $grupo;?>"  data-ng-hide="a.delete">
 
         <div  class="adjunto-wrapper" style="position: relative">
               <span data-ng-click="removeAnexo(a)" style="font-size:30px;cursor: pointer;position: absolute;z-index: 55;top: 5px;right:5px;color: rgba(220, 69, 47, 1)">
