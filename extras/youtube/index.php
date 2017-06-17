d270bcb1dd34c97cbb4376c9b26919fc472ca5e3
gabrielmacus <gabrielmacus@gmail.com>
2017-06-16 23:01:06 -0300

    ()

--- extras/youtube/index.php
+++ extras/youtube/index.php
@@ -7,6 +7,7 @@
  */
 include_once "../../includes/autoload.php";
 
+include "url_parser.php";
 
 // Create DOM from URL or file
 $html = file_get_html("https://www.youtube.com/results?search_query={$_GET["q"]}");
@@ -16,7 +17,9 @@ $html = file_get_html("https://www.youtube.com/results?search_query={$_GET["q"]}
 foreach($html->find('.yt-lockup-title a') as $element)
 {
 
-        var_dump($element->href);
+
+
+        var_dump(VideoUrlParser::get_url_id($element->href));
     var_dump($element->innertext);
 }
 
