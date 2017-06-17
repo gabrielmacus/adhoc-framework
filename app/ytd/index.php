8f379ecfcc880622cc7172e701806fa312d17a51
gabrielmacus <gabrielmacus@gmail.com>
2017-06-17 16:03:10 -0300

    ()

--- app/ytd/index.php
+++ app/ytd/index.php
@@ -3,7 +3,7 @@ require('youtube-dl.class.php');
 try {
 
 
-    new yt_downloader("http://www.youtube.com/watch?v=px17OLxdDMU", TRUE, "audio");
+    new yt_downloader("http://www.youtube.com/watch?v=bHGECrXM5_Q", TRUE, "audio");
 }
 catch (Exception $e) {
     die($e->getMessage());
