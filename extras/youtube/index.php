dbc1c856918404d035aa15d01ea9d9d50c94d805
gabrielmacus <gabrielmacus@gmail.com>
2017-06-16 22:38:23 -0300

    ()

--- extras/youtube/index.php
+++ extras/youtube/index.php
@@ -15,6 +15,3 @@ $html = file_get_html('http://www.google.com/');
 foreach($html->find('img') as $element)
     echo $element->src . '<br>';
 
-// Find all links
-foreach($html->find('a') as $element)
-    echo $element->href . '<br>';
