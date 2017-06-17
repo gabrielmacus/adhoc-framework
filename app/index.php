4dcd887170fea865c20b25653e58beec52c91277
gabrielmacus <gabrielmacus@gmail.com>
2017-06-17 15:59:06 -0300

    ()

--- app/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'PhpTube.php';
-$tube = new PhpTube();
-$videos = $tube->getDownloadLink('https://www.youtube.com/watch?v=uLIs0j2WnlM');
-
-var_dump($videos);
-?>
-                   
---
+++ app/test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Puers
+ * Date: 17/06/2017
+ * Time: 15:58
+ */
+include "youtube-dl.class.php";
+
+try {
+    // Instantly download a YouTube video (using the default settings).
+    new yt_downloader('http://www.youtube.com/watch?v=aahOEZKTCzU', TRUE);
+
+    // Instantly download a YouTube video as MP3 (using the default settings).
+    new yt_downloader('http://www.youtube.com/watch?v=aahOEZKTCzU', TRUE, 'audio');
+}
+catch (Exception $e) {
+    die($e->getMessage());
+}
