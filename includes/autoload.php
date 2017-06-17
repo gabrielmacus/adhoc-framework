48f44aaf02d369a260c6556ca6e539d0105f4ade
gabrielmacus <gabrielmacus@gmail.com>
2017-06-17 11:27:38 -0300

    ()

--- includes/autoload.php
+++ includes/autoload.php
@@ -2,8 +2,16 @@
 
 
 ob_start();//Importante para que cree un buffer antes de enviar
+
+
 //Prueba, borrar en produccion
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, 
+    X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+
+
 /**
  * Created by PhpStorm.
  * User: Gabriel
