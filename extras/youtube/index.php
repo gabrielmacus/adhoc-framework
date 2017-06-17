1de4f4cdde2425669a15cd88b30b1ec48cd58d87
gabrielmacus <gabrielmacus@gmail.com>
2017-06-16 22:30:46 -0300

    ()

---
+++ extras/youtube/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Puers
+ * Date: 16/06/2017
+ * Time: 22:30
+ */
+include_once "../../includes/autoload.php";
+
+var_dump(HTTPrequest("https://www.youtube.com/results?search_query=cerati"));
---
+++ includes/classes/MediaGateway/autoload.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Puers
+ * Date: 16/06/2017
+ * Time: 22:24
+ */
+include "Client/DailymotionClient.php";
+include "Client/FlickrClient.php";
+include "Client/MediaProviderClient.php";
+include "Client/SoundcloudClient.php";
+include "Client/VimeoClient.php";
+include "Client/YoutubeClient.php";
+
+include "Normalizer/DailymotionNormalizer.php";
+include "Normalizer/FlickrNormalizer.php";
+include "Normalizer/SoundcloudNormalizer.php";
+include "Normalizer/VimeoNormalizer.php";
+include "Normalizer/YoutubeNormalizer.php";
+
+include "Model/Vimeo.php";
+include "Model/Dailymotion.php";
+include "Model/Flickr.php";
+include "Model/MediaProvider.php";
+include "Model/Soundcloud.php";
+include "Model/Youtube.php";
+
+
+
+include "Provider/AbstractProvider.php";
+include "Provider/AbstractProvider.php";
