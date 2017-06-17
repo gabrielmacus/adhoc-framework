34df129db75d930bee5da73241d73d3d0eb116b3
gabrielmacus <gabrielmacus@gmail.com>
2017-06-16 22:38:50 -0300

    ()

--- extras/youtube/index.php
+++ extras/youtube/index.php
@@ -9,7 +9,7 @@ include_once "../../includes/autoload.php";
 
 
 // Create DOM from URL or file
-$html = file_get_html('http://www.google.com/');
+$html = file_get_html('https://www.youtube.com/results?search_query=cerati');
 
 // Find all images
 foreach($html->find('img') as $element)
