9839fc033abe42c39e84a80454405d04be48dad2
gabrielmacus <gabrielmacus@gmail.com>
2017-06-17 11:28:25 -0300

    ()

--- includes/autoload.php
+++ includes/autoload.php
@@ -1,12 +1,5 @@
 <?php
 
-//Prueba, borrar en produccion
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credentials: true ");
-header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, 
-    X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
-
 
 ob_start();//Importante para que cree un buffer antes de enviar
 
