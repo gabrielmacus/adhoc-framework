1722229f0f7787e5a5ec574913112b9bbfca8787
gabrielmacus <gabrielmacus@gmail.com>
2017-06-17 15:59:40 -0300

    ()

--- app/test.php
+++ app/test.php
@@ -5,7 +5,7 @@
  * Date: 17/06/2017
  * Time: 15:58
  */
-include "youtube-dl.class.php";
+require ( "youtube-dl.class.php");
 
 try {
     // Instantly download a YouTube video (using the default settings).
