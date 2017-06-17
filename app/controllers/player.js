2dcd439d62915083ff10029e5f029b597513766e
gabrielmacus <gabrielmacus@gmail.com>
2017-06-17 18:19:12 -0300

    ()

--- app/controllers/player.js
+++ app/controllers/player.js
@@ -1,12 +1,20 @@
 
+function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+}
 
     angularApp.controller('playerCtrl', function( $rootScope,$scope,$timeout) {
+        var videoId=youtube_parser($rootScope.song.href);
+
+        console.log(videoId);
 
         var youtubePlayer = new YT.Player('youtubePlayer', {
 
             height: '360',
             width: '640',
-            videoId: $rootScope.song.href
+            videoId: videoId
         });
 
             console.log($rootScope.song);
