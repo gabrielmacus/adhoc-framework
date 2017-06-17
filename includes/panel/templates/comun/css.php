a10791341c2dff155fcfe5cc9f41424cbaf1beba
gabrielmacus <gabrielmacus@gmail.com>
2017-06-16 22:16:15 -0300

    ()

---
+++ includes/panel/templates/comun/css.php
@@ -0,0 +1,19 @@
+
+<!-- Theme included stylesheets -->
+<link href="//cdn.quilljs.com/1.2.4/quill.snow.css" rel="stylesheet">
+<link href="//cdn.quilljs.com/1.2.4/quill.bubble.css" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/estilo.css">
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/lity.min.css">
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/reset.css">
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/font-awesome-4.7.0/css/font-awesome.css">
+<!--<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/ng-tags-input.min.css">-->
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/taggle.min.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/vex.css">
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/vex-theme-plain.css">
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/ng-animate.css">
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/select-theme-default.css">
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/contextMenu.min.css">
+<link rel="stylesheet" href="<?php echo $configuracion->getCssCdn()?>/css/ng-animate.css">
+
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
