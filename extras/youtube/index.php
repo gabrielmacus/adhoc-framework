aa2dbd45aa33f3843b5619605a2c2c504087d85f
gabrielmacus <gabrielmacus@gmail.com>
2017-06-16 22:32:06 -0300

    ()

--- extras/youtube/index.php
+++ extras/youtube/index.php
@@ -7,4 +7,16 @@
  */
 include_once "../../includes/autoload.php";
 
-var_dump(HTTPrequest("https://www.youtube.com/results?search_query=cerati"));
+
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://www.youtube.com/results?search_query=cerati',
+    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+));
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
