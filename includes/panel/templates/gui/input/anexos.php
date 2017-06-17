764d4e8e7f1ac833b5b27bc24fc027142e54f253
gabrielmacus <gabrielmacus@gmail.com>
2017-06-16 21:37:54 -0300

    ()

--- includes/panel/templates/gui/input/anexos.php
+++ includes/panel/templates/gui/input/anexos.php
@@ -191,7 +191,7 @@ if(!$shownText)
     <label class="fila" style="margin-bottom: 10px;"><?php echo $label;?></label>
 
 
-    <ul class="grid"  html-sortable="sortable_option"  data-ng-model="post.anexos" id="sortable<?php echo $grupo;?>" >
+    <ul class="grid"  html-sortable="sortable_option"  data-ng-model="post.anexos<?php echo $grupo;?>" id="sortable<?php echo $grupo;?>" >
         <li  data-idx="{{k}}" class="s12 m6 l4 padding item" data-ng-repeat="(k,a) in  post.anexos" data-ng-if="a.post_nexo_grupo==<?php echo $grupo;?>"  data-ng-hide="a.delete">
 
             <div  class="adjunto-wrapper" style="position: relative">
