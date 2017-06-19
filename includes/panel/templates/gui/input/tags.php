86487da7943678f5b2f63c3637c6a8eca15a954a
Gabriel <gabrielmacus@gmail.com>
2017-06-19 12:09:08 -0300

    ()

--- includes/panel/templates/gui/input/tags.php
+++ includes/panel/templates/gui/input/tags.php
@@ -7,6 +7,55 @@
 
         angular.element(function () {
 
+            <?php
+            if($regex)
+            {
+            ?>
+
+            if (!scope.validation) {
+                scope.validation = {};
+            }
+
+
+            scope.validation.<?php echo $model?>= {
+                isValid: true, check: function () {
+
+
+                    var pattern=/<?php echo $regex?>/g;
+
+                    var arr=JSON.parse(scope.post.<?php echo $model?>);
+
+                    $.each(arr,function (clave,valor) {
+                        
+                        if(!pattern.test(valor) || !valor)
+                        {
+                            scope.validation.<?php echo $model?>.isValid=false;
+
+                            return false;
+                        }
+                        else
+                        {
+                            scope.validation.<?php echo $model?>.isValid=true;
+                        }
+
+                    });
+
+
+                    setTimeout(function () {
+                        scope.$apply();
+                    });
+
+
+                }
+            };
+            $(document).on("change","[data-ng-model='post.<?php echo $model?>']",function () {
+
+                scope.validation.<?php echo $model?>.check();
+            });
+
+            <?php
+            }?>
+
 
             var tags=[];
 
@@ -51,8 +100,11 @@ if(!tags)
                 duplicateTagClass: 'bounce',
                 placeholder:"<?php echo $placeholder?>",
                 onTagAdd:function () {
-
+                    
                     scope.post.<?php echo $model?>  = JSON.stringify(<?php echo $model?>.getTags().values);
+
+                    scope.validation.<?php echo $model?>.check();
+             
                 },
                 onTagRemove:function () {
 
@@ -65,3 +117,9 @@ if(!tags)
     <label><?php echo $label;?></label>
     <div style= "border: 1px #9ba096 solid;" class="tags" id="<?php echo $id?>"></div>
 </div>
+<?php
+if($regex) {
+    include "error.php";
+}
+?>
+
