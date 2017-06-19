cdeab49bd886b3e4b7a91ae801ab15d2a3eca0f7
Gabriel <gabrielmacus@gmail.com>
2017-06-19 11:51:13 -0300

    ()

--- includes/panel/templates/gui/input/select.php
+++ includes/panel/templates/gui/input/select.php
@@ -1,9 +1,62 @@
+
 <script>
     angular.element(function () {
 
         scope.<?php echo $model?>=<?php echo json_encode($options);?>;
+
+        <?php
+        if($regex)
+        {
+        ?>
+
+        if (!scope.validation) {
+            scope.validation = {};
+        }
+
+
+        scope.validation.<?php echo $model?>= {
+            isValid: true, check: function () {
+
+
+                var pattern=/<?php echo $regex?>/g;
+
+
+                if(!pattern.test(scope.post.<?php echo $model?>) || !scope.post.<?php echo $model?>)
+                {
+                    scope.validation.<?php echo $model?>.isValid=false;
+                }
+                else
+                {
+                    scope.validation.<?php echo $model?>.isValid=true;
+                }
+
+                setTimeout(function () {
+                    scope.$apply();
+                });
+
+
+            }
+        };
+
+        <?php
+        }?>
+
+
+
+
+
+
+
     });
+
+    $(document).on("change","[data-ng-model='post.<?php echo $model?>']",function () {
+
+        scope.validation.<?php echo $model?>.check();
+    });
+
 </script>
+
+
 <div title="<?php echo $label?>"  class="form-block <?php echo implode(" ",$class);?>">
     <label><?php echo $label?></label>
     <select  data-ng-options="item for item in <?php echo $model?>"
@@ -11,3 +64,9 @@
     >
     </select>
 </div>
+<?php
+if($regex) {
+    include "error.php";
+}
+?>
+
