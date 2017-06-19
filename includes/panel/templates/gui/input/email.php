ca0b26b79fe63903163a00709251a95774a8f243
Gabriel <gabrielmacus@gmail.com>
2017-06-19 10:35:20 -0300

    ()

--- includes/panel/templates/gui/input/email.php
+++ includes/panel/templates/gui/input/email.php
@@ -1,5 +1,61 @@
+<script>
+    angular.element(function () {
 
+
+
+        <?php
+        if($regex)
+        {
+        ?>
+
+        if (!scope.validation) {
+            scope.validation = {};
+        }
+
+
+        scope.validation.<?php echo $model?>= {
+            isValid: true, check: function () {
+
+
+                var pattern=/<?php echo $regex?>/g;
+
+
+                if(!pattern.test(scope.post.<?php echo $model?>) || !scope.post.<?php echo $model?>)
+                {
+                    scope.validation.<?php echo $model?>.isValid=false;
+                }
+                else
+                {
+                    scope.validation.<?php echo $model?>.isValid=true;
+                }
+
+                setTimeout(function () {
+                    scope.$apply();
+                });
+
+
+            }
+        };
+
+        <?php
+        }?>
+
+    });
+
+    $(document).on("input","[data-ng-model='post.<?php echo $model?>']",function () {
+
+        scope.validation.<?php echo $model?>.check();
+    });
+
+</script>
 <div class="form-block <?php echo implode(" ",$class);?>">
     <label><?php echo $label?></label>
-    <input type="email" title="<?php echo $label?>" placeholder="<?php echo $placeholder?>" data-ng-model="post.<?php echo $model?>" maxlength="<?php echo $max?>" minlength="<?php echo $min;?>">
+    <input type="email" title="<?php echo $label?>" placeholder="<?php echo $placeholder?>" data-ng-model="post.<?php echo $model?>" >
 </div>
+
+<?php
+if($regex) {
+    include "error.php";
+}
+?>
+
--- includes/panel/templates/gui/input/number.php
+++ includes/panel/templates/gui/input/number.php
@@ -1,4 +1,64 @@
+<script>
+    angular.element(function () {
+
+
+
+        <?php
+        if($regex)
+        {
+        ?>
+
+        if (!scope.validation) {
+            scope.validation = {};
+        }
+
+
+        scope.validation.<?php echo $model?>= {
+            isValid: true, check: function () {
+
+
+                var pattern=/<?php echo $regex?>/g;
+
+
+                if(!pattern.test(scope.post.<?php echo $model?>) || !scope.post.<?php echo $model?>)
+                {
+                    scope.validation.<?php echo $model?>.isValid=false;
+                }
+                else
+                {
+                    scope.validation.<?php echo $model?>.isValid=true;
+                }
+
+                setTimeout(function () {
+                    scope.$apply();
+                });
+
+
+            }
+        };
+
+        <?php
+        }?>
+
+    });
+
+    $(document).on("input","[data-ng-model='post.<?php echo $model?>']",function () {
+
+        scope.validation.<?php echo $model?>.check();
+    });
+
+</script>
 <div class="form-block <?php echo implode(" ",$class);?>">
     <label><?php echo $label?></label>
-    <input string-to-number type="number" title="<?php echo $label?>" placeholder="<?php echo $placeholder?>" data-ng-model="post.<?php echo $model?>"  max="<?php echo $max?>" min="<?php echo $min;?>">
+    <input string-to-number type="number" title="<?php echo $label?>" placeholder="<?php echo $placeholder?>" data-ng-model="post.<?php echo $model?>"  >
 </div>
+
+<?php
+if($regex) {
+    include "error.php";
+}
+?>
+
+
+
+
