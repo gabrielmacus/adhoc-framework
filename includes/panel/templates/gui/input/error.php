2f857a74c092271fd841238ec58689db8581052c
gabrielmacus <gabrielmacus@gmail.com>
2017-06-18 22:40:43 -0300

    ()

--- includes/panel/templates/gui/input/error.php
+++ includes/panel/templates/gui/input/error.php
@@ -1,3 +1,13 @@
+
+<?php
+
+if(!$errorMsg)
+{
+    $errorMsg="Complete el campo correctamente";
+}
+
+?>
+
 <div class="validation-error" data-ng-if="!validation.<?php echo $model;?>.isValid">
     <span class="text"><?php echo $errorMsg;?></span>
 </div>
--- includes/panel/templates/gui/input/text.php
+++ includes/panel/templates/gui/input/text.php
@@ -1,5 +1,57 @@
+<script>
+    angular.element(function () {
 
+
+
+        <?php
+        if($regex)
+        {
+        ?>
+
+        if (!scope.validation) {
+            scope.validation = {};
+        }
+
+
+        scope.validation.<?php echo $model?>= {
+            isValid: true, check: function () {
+
+
+                $(".secciones select").each(
+                    function () {
+
+                        if ($(this).val() == "") {
+                            scope.validation.secciones.isValid = false;
+                            return false;
+                        }
+                        else {
+                            scope.validation.secciones.isValid = true;
+                        }
+
+                    }
+                );
+
+                setTimeout(function () {
+                    scope.$apply();
+                });
+
+
+            }
+        };
+
+        <?php
+        }?>
+
+    });
+
+</script>
 <div class="form-block <?php echo implode(" ",$class);?>">
     <label><?php echo $label?></label>
     <input type="text" title="<?php echo $label?>" placeholder="<?php echo $placeholder?>" data-ng-model="post.<?php echo $model?>" maxlength="<?php echo $max?>" minlength="<?php echo $min;?>">
 </div>
+
+<?php
+$model="secciones";
+include "error.php";
+
+?>
