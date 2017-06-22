fe08b1cef8b19fddf63373f05276d2f06622107b
Gabriel <gabrielmacus@gmail.com>
2017-06-22 14:45:42 -0300

    ()

--- includes/panel/templates/archivos/list.php
+++ includes/panel/templates/archivos/list.php
@@ -182,7 +182,7 @@
             </div>
 
             <!-- videos sindicados -->
-            <div  class="file-preview s12 m4 l3" data-ng-repeat="p in previews" data-ng-if="p.type=='youtube'">
+            <div  class="file-preview s12 m4 l3" data-ng-repeat="p in previews" data-ng-if="p.type=='youtube' || p.type=='vimeo'">
                 <!-- data-ng-if="p.type=='jpg' || p.type=='jpeg' ||p.type=='gif' ||p.type=='jpg' ||p.type=='png'"-->
                 <div  class="file" >
 
