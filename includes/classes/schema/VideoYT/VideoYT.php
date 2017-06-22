7ae909cf3e8d9a5586ea267cf081d44997b3cdb1
Gabriel <gabrielmacus@gmail.com>
2017-06-22 12:07:07 -0300

    ()

--- includes/classes/schema/VideoYT/VideoYT.php
+++ includes/classes/schema/VideoYT/VideoYT.php
@@ -13,6 +13,12 @@ class VideoYT extends Archivo
         parent::__construct($size, $name, $mime, $version, $realName, $tmpPath, $repositorio, $path, $creation, $modification, $id, $versionName, 5);
     }
 
-    
+
+    function getId()
+    {
+        $url = $this->getRealName();
+        parse_str( parse_url( $url, PHP_URL_QUERY ), $my_array_of_vars );
+        return $my_array_of_vars['v'];
+    }
 
 }
