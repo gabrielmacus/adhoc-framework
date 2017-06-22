916964bbf6e484b16369ec78e9a285f57cbfdf08
Gabriel <gabrielmacus@gmail.com>
2017-06-22 14:36:08 -0300

    ()

---
+++ extras/vimeo/get_info.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once "../../includes/autoload.php";
+
+
+// el parametro sp es para que solo me traiga videos
+$html = file_get_html("{$_GET["url"]}");
+
+$html->find("script[type='application/ld+json']",0)->innertext;
+
