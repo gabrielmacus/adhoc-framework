6eee1d5a351729f7ac264fca1bf2a3837da82aa8
Gabriel <gabrielmacus@gmail.com>
2017-06-23 12:02:48 -0300

    ()

--- includes/site/templates/home/objetos/top.php
+++ includes/site/templates/home/objetos/top.php
@@ -18,24 +18,27 @@ foreach ($subsecciones as $k=>$v)
         <div class="mask fit animated">
 
         </div>
-        <figure class="fit valign flex center">
-            <img class="fit " src="<?php echo $s["img"]?>">
-            <figcaption>
-                <p>
-                    <?php
-                    if(!$s["text"])
-                    {
-                        $txt =$v->getNombre();
-                    }
-                    else
-                    {
-                        $txt= $s["text"];
-                    }
-                    echo $txt;
-                    ?>
-                </p>
-            </figcaption>
-        </figure>
+        <a>
+            <figure class="fit valign flex center">
+                <img class="fit " src="<?php echo $s["img"]?>">
+                <figcaption>
+                    <p>
+                        <?php
+                        if(!$s["text"])
+                        {
+                            $txt =$v->getNombre();
+                        }
+                        else
+                        {
+                            $txt= $s["text"];
+                        }
+                        echo $txt;
+                        ?>
+                    </p>
+                </figcaption>
+            </figure>
+        </a>
+
        
     </div>
     <?php
