e866ca049509a6b562da32e6dc49680d77ec2a64
gabrielmacus <gabrielmacus@gmail.com>
2017-06-24 13:26:36 -0300

    ()

--- includes/site/templates/home/objetos/bloque-1.php
+++ includes/site/templates/home/objetos/bloque-1.php
@@ -4,5 +4,11 @@
         <h2>Últimos clasificados</h2>
     </header>
 
+    <article class="item">
+        <figure>
+            <img src="http://clasionce-media.elonce.com/2017/06/22/1498179809_130420_0_m.jpg">
+        </figure>
+    </article>
+
 
 </div>
