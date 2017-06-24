972aeb70f2f416301d8d0f44c98f78edf3737702
gabrielmacus <gabrielmacus@gmail.com>
2017-06-24 13:30:55 -0300

    ()

--- includes/site/templates/home/objetos/bloque-1.php
+++ includes/site/templates/home/objetos/bloque-1.php
@@ -9,10 +9,11 @@
             <figure>
                 <img class="fit" src="http://clasionce-media.elonce.com/2017/06/22/1498179809_130420_0_m.jpg">
             </figure>
+            <div class="text">
+                <p>PICK UP BARATA OROCH DYNA 1.6 $ 57.100 y TASA BAJA O TASA 0%</p>
+            </div>
         </article>
-        <div class="text">
-            <p>PICK UP BARATA OROCH DYNA 1.6 $ 57.100 y TASA BAJA O TASA 0%</p>
-        </div>
+
     </div>
 
 
