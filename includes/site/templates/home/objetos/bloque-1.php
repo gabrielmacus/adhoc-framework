40fd1dc5a443d211903c9baa7a4dc41bd8dabde8
gabrielmacus <gabrielmacus@gmail.com>
2017-06-24 13:38:02 -0300

    ()

--- includes/site/templates/home/objetos/bloque-1.php
+++ includes/site/templates/home/objetos/bloque-1.php
@@ -4,18 +4,26 @@
         <h2>Últimos clasificados</h2>
     </header>
 
-    <div class="body flex">
-        <article class="item flex">
-            <figure>
-                <img class="fit" src="http://www.carsstories.com/wp-content/uploads/2017/05/car-searsh-sites.jpg">
-            </figure>
-            <div class="text">
-                <p>PICK UP BARATA OROCH DYNA 1.6 $ 57.100 y TASA BAJA O TASA 0%</p>
-            </div>
-        </article>
+    <?php
+    for ($i=0;$i<6;$i++)
+    {
+        ?>
 
-    </div>
+        <div class="body flex">
+            <article class="item flex">
+                <figure>
+                    <img class="fit" src="http://www.carsstories.com/wp-content/uploads/2017/05/car-searsh-sites.jpg">
+                </figure>
+                <div class="text">
+                    <p>PICK UP BARATA OROCH DYNA 1.6 $ 57.100 y TASA BAJA O TASA 0%</p>
+                </div>
+            </article>
 
+        </div>
+
+        <?php
+    }
+    ?>
 
 
 </div>
