80e176eb770f0a911bf6573b4696830e7f2b15bd
gabrielmacus <gabrielmacus@gmail.com>
2017-06-25 19:09:34 -0300

    ()

--- includes/site/templates/home/objetos/bloque-2.php
+++ includes/site/templates/home/objetos/bloque-2.php
@@ -16,9 +16,6 @@
                             <img class="fit" src="<?php echo $img[rand(0,(count($img)-1))];?>">
                         </figure>
 
-                        <div class="mask main-background-color">
-
-                        </div>
                     </div>
 
                     <?php
