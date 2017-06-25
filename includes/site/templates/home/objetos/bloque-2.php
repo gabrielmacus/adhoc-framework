4ffbc8c0211b8ae933b7fca889b2b5a2b0aa1f3e
gabrielmacus <gabrielmacus@gmail.com>
2017-06-25 19:16:14 -0300

    ()

--- includes/site/templates/home/objetos/bloque-2.php
+++ includes/site/templates/home/objetos/bloque-2.php
@@ -4,9 +4,9 @@
         <h2>Lo más visto</h2>
     </header>
 
-    <div class="slider-wrapper swiper-container">
+    <div class=" swiper-container">
 
-        <div class="slider-container flex swiper-wrapper">
+        <div class="swiper-wrapper">
 
                 <?php  for ($i=0;$i<9;$i++)
                 {
@@ -27,23 +27,6 @@
     </div>
 
 
-</div>
-<!-- Swiper -->
-<div class="swiper-container">
-    <div class="swiper-wrapper">
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 4</div>
-        <div class="swiper-slide">Slide 5</div>
-        <div class="swiper-slide">Slide 6</div>
-        <div class="swiper-slide">Slide 7</div>
-        <div class="swiper-slide">Slide 8</div>
-        <div class="swiper-slide">Slide 9</div>
-        <div class="swiper-slide">Slide 10</div>
-    </div>
-    <!-- Add Pagination -->
-    <div class="swiper-pagination"></div>
 </div>
 <script>
     $(document).ready(
