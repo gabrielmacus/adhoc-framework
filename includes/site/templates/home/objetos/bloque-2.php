bc83242b61b3add24fadff9c2f60f672905c3b02
gabrielmacus <gabrielmacus@gmail.com>
2017-06-25 18:58:17 -0300

    ()

--- includes/site/templates/home/objetos/bloque-2.php
+++ includes/site/templates/home/objetos/bloque-2.php
@@ -4,27 +4,30 @@
         <h2>Lo más visto</h2>
     </header>
 
+    <div class="slider-wrapper">
 
-    <div class="slider flex">
+        <div class="slider flex">
 
-        <?php  for ($i=0;$i<9;$i++)
-        {
-            ?>
-            <div class="image">
-                <figure>
-                    <img class="fit" src="<?php echo $img[rand(0,(count($img)-1))];?>">
-                </figure>
-                <div class="mask main-background-color">
-                    <div class="text">
-                        <p>PICK UP BARATA OROCH DYNA 1.6 $ 57.100 y TASA BAJA O TASA 0%</p>
+            <?php  for ($i=0;$i<9;$i++)
+            {
+                ?>
+                <div class="image">
+                    <figure>
+                        <img class="fit" src="<?php echo $img[rand(0,(count($img)-1))];?>">
+                    </figure>
+                    <div class="mask main-background-color">
+                        <div class="text">
+                            <p>PICK UP BARATA OROCH DYNA 1.6 $ 57.100 y TASA BAJA O TASA 0%</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <?php
-        }?>
+                <?php
+            }?>
 
 
+        </div>
     </div>
+
 
 </div>
