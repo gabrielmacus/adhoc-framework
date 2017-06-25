9fa2f0a2a261923664e138bf24776d0612788e59
gabrielmacus <gabrielmacus@gmail.com>
2017-06-25 19:25:44 -0300

    ()

--- includes/site/templates/home/objetos/bloque-2.php
+++ includes/site/templates/home/objetos/bloque-2.php
@@ -1,8 +1,9 @@
+<header class="top main-color">
+    <h2>Lo más visto</h2>
+</header>
 <div class="mas-vistos flex">
 
-    <header class="top main-color">
-        <h2>Lo más visto</h2>
-    </header>
+
 
 
 
