4383557903e57f16d665781d106752287a33fa60
Gabriel <gabrielmacus@gmail.com>
2017-06-26 09:56:43 -0300

    ()

--- includes/site/templates/login/index.php
+++ includes/site/templates/login/index.php
@@ -1,3 +1,24 @@
-<div class="body">
-    <h2>Demo</h2>
+
+<header class="top">
+    <h2>Ingresá a tu cuenta</h2>
+</header>
+<div class="body" data-ng-submit="onLogin()">
+
+    <div class="flex">
+        <label>
+            Usuario o Email
+        </label>
+        <input data-ng-model="usuario.nick" type="text" >
+    </div>
+    <div class="flex">
+        <label>
+            Contraseña
+        </label>
+        <input data-ng-model="usuario.password" type="password" >
+    </div>
+
+    <div class="flex">
+        <button type="submit">Ingresar</button>
+    </div>
+
 </div>
