13d4a4ba01091c1ba30790bd58274a9bcf9ec9d8
gabrielmacus <gabrielmacus@gmail.com>
2017-06-26 20:34:13 -0300

    ()

--- includes/classes/schema/Usuario/UserDAO.php
+++ includes/classes/schema/Usuario/UserDAO.php
@@ -168,7 +168,7 @@ usuario_surname=:usuario_surname,usuario_creation=:usuario_creation,usuario_modi
 
 
         $user =$this->users[0];
-            var_dump($user);
+            var_dump($user->getId());
 ///                'exp'  => time()+3600,           // Expire (en una hora)
         if($user)
         {
