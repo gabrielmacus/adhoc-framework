ff0e000d611252c3b5b16f4b2ccbc9fd2ba67282
Gabriel <gabrielmacus@gmail.com>
2017-06-26 11:29:36 -0300

    ()

--- includes/site/templates/login/index.php
+++ includes/site/templates/login/index.php
@@ -1,17 +1,17 @@
 
-<div class="main-form">
+
     <header class="top center">
         <h2>Ingresá a tu cuenta</h2>
     </header>
     <form class="body" data-ng-submit="onLogin()">
 
-        <div class="flex">
+        <div class="flex form-field">
             <label>
                 Usuario o Email
             </label>
             <input data-ng-model="usuario.nick" type="text" >
         </div>
-        <div class="flex">
+        <div class="flex form-field">
             <label>
                 Contraseña
             </label>
@@ -23,4 +23,3 @@
         </div>
 
     </form>
-</div>
