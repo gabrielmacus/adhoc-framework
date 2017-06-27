f0a157fe811721a15fbd81c8995e2d9d2572b2be
Gabriel <gabrielmacus@gmail.com>
2017-06-27 10:16:57 -0300

    ()

--- includes/panel/templates/comun/loader.php
+++ includes/panel/templates/comun/loader.php
@@ -8,129 +8,54 @@
     });
     
 </script>
-<div class="showbox">
-    <div class="loader">
-        <svg class="circular" viewBox="25 25 50 50">
-            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
-        </svg>
-    </div>
+<div class="loader">
+    <svg class="circular">
+        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+    </svg>
 </div>
+
 <style>
-    body {
-        background-color: #eee;
-    }
-
-    .showbox {
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        padding: 5%;
-    }
-
-    .loader {
+    loader {
         position: relative;
-        margin: 0 auto;
+        margin: 0px auto;
         width: 100px;
-    }
-    .loader:before {
-        content: '';
-        display: block;
-        padding-top: 100%;
+        height: 100px;
+        zoom: 1;
     }
 
     .circular {
-        -webkit-animation: rotate 2s linear infinite;
-        animation: rotate 2s linear infinite;
-        height: 100%;
-        -webkit-transform-origin: center center;
-        transform-origin: center center;
-        width: 100%;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        margin: auto;
+        animation: rotate 1s linear infinite;
+        height: 100px;
+        position: relative;
+        width: 100px;
     }
 
+
     .path {
-        stroke-dasharray: 1, 200;
+        stroke: gray;
+        stroke-dasharray: 1,200;
         stroke-dashoffset: 0;
-        -webkit-animation: dash 1.5s ease-in-out infinite, color 6s ease-in-out infinite;
-        animation: dash 1.5s ease-in-out infinite, color 6s ease-in-out infinite;
+        animation: dash 1.5s ease-in-out infinite;
         stroke-linecap: round;
     }
 
-    @-webkit-keyframes rotate {
-        100% {
-            -webkit-transform: rotate(360deg);
-            transform: rotate(360deg);
-        }
-    }
-
     @keyframes rotate {
-        100% {
-            -webkit-transform: rotate(360deg);
+        100%{
             transform: rotate(360deg);
         }
     }
-    @-webkit-keyframes dash {
-        0% {
-            stroke-dasharray: 1, 200;
+    @keyframes dash{
+        0%{
+            stroke-dasharray: 1,200;
             stroke-dashoffset: 0;
         }
-        50% {
-            stroke-dasharray: 89, 200;
-            stroke-dashoffset: -35px;
+        50%{
+            stroke-dasharray: 89,200;
+            stroke-dashoffset: -35;
         }
-        100% {
-            stroke-dasharray: 89, 200;
-            stroke-dashoffset: -124px;
-        }
-    }
-    @keyframes dash {
-        0% {
-            stroke-dasharray: 1, 200;
-            stroke-dashoffset: 0;
-        }
-        50% {
-            stroke-dasharray: 89, 200;
-            stroke-dashoffset: -35px;
-        }
-        100% {
-            stroke-dasharray: 89, 200;
-            stroke-dashoffset: -124px;
+        100%{
+            stroke-dasharray: 89,200;
+            stroke-dashoffset: -124;
         }
     }
-    @-webkit-keyframes color {
-        100%, 0% {
-            stroke: #d62d20;
-        }
-        40% {
-            stroke: #0057e7;
-        }
-        66% {
-            stroke: #008744;
-        }
-        80%, 90% {
-            stroke: #ffa700;
-        }
-    }
-    @keyframes color {
-        100%, 0% {
-            stroke: #d62d20;
-        }
-        40% {
-            stroke: #0057e7;
-        }
-        66% {
-            stroke: #008744;
-        }
-        80%, 90% {
-            stroke: #ffa700;
-        }
-    }
-
 </style>
