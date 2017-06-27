12513ccaa435a12c468f6ad06b936ac26a68d326
Gabriel <gabrielmacus@gmail.com>
2017-06-27 11:38:09 -0300

    ()

--- js/facebook.js
+++ js/facebook.js
@@ -16,7 +16,7 @@ angular.element(function () {
                 scope.solicitarPermisos();
             }
             else {
-                FB.login(facebookReady, {scope: scope.facebookData.permissions});
+                FB.login(facebookReady, {scope: scope.facebookData.permissions, return_scopes: true});
             }
         });
 
@@ -63,16 +63,7 @@ angular.element(function () {
 
     function facebookReady(e) {
 
-        FB.api(
-            "/me",
-            function (response) {
-
-                console.log(response);
-
-
-            }
-        );
-
+        console.log(e);
 //    publicarEstado("Jugando al pes",[100001209271475]);
 
         //  publicarImagenEnGrupo("https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-0/p180x540/17630114_1368806563173057_6365164914191947421_n.jpg?oh=2b2410f12f57012c520d8238b15be13d&oe=5953A000","Vendo secador, muy poco uso, excelente estado $500",189905047763101);
